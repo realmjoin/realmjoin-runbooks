40a92c4fe6b52693f4c16da1afc7fcf45b41cdf7
Jan Berdel <jan.berdel@glueckkanja.com>
2020-07-30 15:34:03 +0200

    Add Org params

--- org/hello-world.ps1
+++ org/hello-world.ps1
@@ -1,7 +1,11 @@
 param
 (
     [Parameter(Mandatory = $true)]
+    [String] $OrganizationID,
+    [Parameter(Mandatory = $true)]
+    [String] $OrganizationName,
+    [Parameter(Mandatory = $true)]
     [String] $CallerName
 )
 
-"Hello org!. I was called by $CallerName."
+"Hello $OrganizationName ($OrganizationID)!. I was called by $CallerName."
