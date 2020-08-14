3772cbb18b50ea2c59d14b4ec69d87fd0fb4a791
Christian Kanja <christian.kanja@glueckkanja.com>
2020-08-14 11:59:54 +0200

    Update hello-world.ps1
    Samples for UI custom types

--- users/hello-world.ps1
+++ users/hello-world.ps1
@@ -5,7 +5,17 @@ param
     [Parameter(Mandatory = $true)]
     [String] $UserName,
     [Parameter(Mandatory = $true)]
-    [String] $CallerName
+    [String] $CallerName,
+    [Parameter(Mandatory = $false)]
+    [String] $AADGroup_ID,
+    [Parameter(Mandatory = $false)]
+    [String] $AADDevice_ID,
+    [Parameter(Mandatory = $false)]
+    [String] $Date_StartDate,
+    [Parameter(Mandatory = $false)]
+    [String] $Date_EndDate,
+    [Parameter(Mandatory = $false)]
+    [String] $Time_StartTime    
 )
 
 "Hello $UserName!. I was called by $CallerName."
