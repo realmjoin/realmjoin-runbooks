6d0e2094f182d26c5144f7af2a1ead90f1d6637b
Jan Berdel <jan.berdel@glueckkanja-gab.com>
2020-10-19 14:36:17 +0200

    Demo

--- group/hello-world.ps1
+++ group/hello-world.ps1
@@ -9,3 +9,9 @@ param
 )
 
 "Hello $GroupName ($GroupID)!. I was called by $CallerName."
+
+Write-Output "Test: Write-Output"
+Write-Warning "Test: Write-Warning"
+Write-Error "Test: Write-Error"
+Write-Verbose "Test: Write-Verbose"
+Write-Progress "Test: Write-Progress"
