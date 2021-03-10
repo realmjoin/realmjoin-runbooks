7e390dd80b94de9df319573dd166a2767138bf8b
Karsten Kleinschmidt <31703575+KarstenKleinschmidt@users.noreply.github.com>
2021-03-10 18:15:26 +0100

    Update add-additional-alias.ps1

--- user/general/add-additional-alias.ps1
+++ user/general/add-additional-alias.ps1
@@ -1,27 +1,33 @@
+#Requires -Modules Az.Accounts
+
 param
 (
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $false)]
     [string] $OrganizationInitialDomainName,
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $false)]
     [string] $OrganizationId,
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $false)]
     [string] $UserName,
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $false)]
     [string] $CallerName,
     [Parameter(Mandatory = $true)]
     [string] $Additional_Alias
 )
 
-Set-StrictMode -Version Latest
+#Set-StrictMode -Version Latest
 #$ErrorActionPreference = "Stop"
 #$ProgressPreference = "SilentlyContinue"
-$GLOBAL:DebugPreference="Continue"  
+#$GLOBAL:DebugPreference="Continue"  
 
+$OrganizationId = "81a3ef22-4447-4627-91ae-6774f48748d1"
 $connectionName = "AzureRunAsConnection"
 $LogType = "AutomationAppConnecting"
 $ErrorMessage = ""
 $Exception = ""
 $desc = ""
+
+$connectionname = "AzureRunAsConnection"
+$appCredentials = Get-AutomationPSCredential -Name 'rj-serviceprincipal'
 
 try
 {
@@ -46,32 +52,17 @@ try
         throw $Exception
     }
 } finally {
-    # Log the Run Book Started
-    #$customLogCollection = New-Object System.Collections.ArrayList
     $desc += "AzureAPPID:[$AzureAPPID] in TenantId:[$tenantId], Sub ID[$subID]. Errors:[$ErrorMessage] Exception:[$Exception]"
-    #$muteOnScreen = $customLogCollection.Add($customLog)
     $desc
+    $Error.Clear()
 }
 
-Write-Output "Add an initial e-Mail address (Alias) initialized by $CallerName for $UserName"
-
-#Debug Connect-AzAccount @Connection -ServicePrincipal 5>&1
-#Connect-AzAccount @servicePrincipalConnection -ServicePrincipal 
-
-Write-Output "After Connect-azaccount"  $Error.Count
-$Error
-
-
-#$appCredentials = Get-AutomationPSCredential -Name 'rj-serviceprincipal'
-
-#Connect-ExchangeOnline -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint -AppId $servicePrincipalConnection.ApplicationId -Organization $OrganizationInitialDomainName 
 
 Function Get-GraphResponse {
     param(
         [parameter(Mandatory = $true)]
         $Url
-    )
-    try {
+        )
         $tokenEndpoint = "https://login.microsoftonline.com/$OrganizationId/oauth2/v2.0/token"
         $body = @{
             grant_type    = "client_credentials"
@@ -89,46 +80,28 @@ Function Get-GraphResponse {
         $header = @{
             'Authorization' = "Bearer $AccessToken"
         }
-    }
-    catch {
-        throw $_.Exception
-    }
-    try {
+        $Url
         $response = Invoke-RestMethod -Uri $Url -Headers $header -Method Get -ContentType "application/json"
-        return $response
-    }catch {
-        return $_.Exception.Response.StatusCode.value__
-    }
+        return $response.value
 }
 
-Write-Output "before IsExchangeAliasUnique "
-$Error.Count
 
 Function IsExchangeAliasUnique($exchangeAlias) {
 #    Search all aliases
     Write-Output "before getgraph"
-    $graph=    Get-GraphResponse -Url "https://graph.microsoft.com/beta/users?`$filter=proxyAddresses/any(x:startswith(x, 'smtp:$Additional_Alias')) or userPrincipalName eq '$Additional_Alias' or mail eq '$Additional_Alias')"
+    $graph= Get-GraphResponse -Url "https://graph.microsoft.com/beta/users?`$filter=proxyAddresses/any(x:startswith(x, 'smtp:$Additional_Alias')) or userPrincipalName eq '$Additional_Alias' or mail eq '$Additional_Alias')"
     Write-Output $graph
     Write-Output "getgraph output"
-
-    Get-GraphResponse -Url "https://graph.microsoft.com/beta/users?`$filter=proxyAddresses/any(x:startswith(x, 'smtp:$Additional_Alias')) or userPrincipalName eq '$Additional_Alias' or mail eq '$Additional_Alias')"
-
-#    if($users.Count -eq 0)
-#    {
-#       return $True
-#    }
-#    return $False
-#
+    #Get-GraphResponse -Url "https://graph.microsoft.com/beta/users?`$filter=proxyAddresses/any(x:startswith(x, 'smtp:$Additional_Alias')) or userPrincipalName eq '$Additional_Alias' or mail eq '$Additional_Alias')"
 }
 
-$Error.Count
+
 
 if (!$Error) {
     Write-Output "Connection to Exchange Online Powershell established!"    
     if (IsExchangeAliasUnique($Additional_Alias)) {
         Write-Output "New Alias will be added to $UserName"
-        $Error.Clear();
-        Set-RemoteMailBox -Identity $UserName -EmailAddresses @{Add='$Additional_Alias'}
+        #Set-RemoteMailBox -Identity $UserName -EmailAddresses @{Add='$Additional_Alias'}
         if (!$Error) {
             Write-Output "New Alias $Additional_Alias was added for $UserName"
         }
@@ -140,10 +113,10 @@ if (!$Error) {
         Write-Output "Alias $Additional_Alias is not unique!"
         $Error.Clear();          
     }
-    Write-Output "Adjusted Out Of Settings for $UserName"
+    Write-Output "Added additional alias  for $UserName"
 }
 else {
-    Write-Error "99 Connection to Exchange Online failed! `r`n $Error"
+    Write-Error "Connection to Exchange Online failed! `r`n $Error"
 }
 
 Write-Output "Disconnect from EXO"
@@ -152,3 +125,4 @@ Get-PsSession | Where-Object {$_.ConfigurationName -eq 'Microsoft.Exchange'} | R
 Disconnect-ExchangeOnline -Confirm:$false
 
 Write-Output "script ended."
+
