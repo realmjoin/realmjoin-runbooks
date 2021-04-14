f4d7a5fb193561d958202405cf1eed439ecce81e
Hans-Carl Oberdalhoff <Hans-Carl.Oberdalhoff@glueckkanja-gab.com>
2021-04-14 18:47:17 +0200

    allow to wait for successful module deployment

--- setup/import-module-from-gallery.ps1
+++ setup/import-module-from-gallery.ps1
@@ -1,37 +1,41 @@
 # Imports a module from PSGallery.
 #
 # requires the Automation Account to have variables created by the "initial setup" runbook.
+#
+# You can call this runbook from other runbooks via 
+# Start-AutomationRunbook -Name "rjgit-setup_import-module-from-gallery" -Parameters @{"moduleName"="..."}"
+
+
 
 param(
     [Parameter(Mandatory = $true)]
-    [string]$moduleName
+    [string]$moduleName,
+    # by default the runbook will exit when deployment is started. Set this to wait for completion of the module deployment.
+    [switch]$waitForDeployment
 )
 
 # Adapted from https://community.idera.com/database-tools/powershell/powertips/b/tips/posts/getting-latest-powershell-gallery-module-version
-function Get-PublishedModuleVersion
-{
+function Get-PublishedModuleVersion {
     param(
         [string]$Name
     )
-   # access the main module page
-   $url = "https://www.powershellgallery.com/packages/$Name"
-   $request = [System.Net.WebRequest]::Create($url)
-   # do not allow to redirect. The result is a "MovedPermanently"
-   $request.AllowAutoRedirect=$false
-   try
-   {
-     # send the request
-     $response = $request.GetResponse()
-     # get back the URL of the true destination page, and split off the version
-     $response.GetResponseHeader("Location").Split("/")[-1]
-     # make sure to clean up
-     $response.Close()
-     $response.Dispose()
-   }
-   catch
-   {
-     Write-Error $_.Exception.Message
-   }
+    # access the main module page
+    $url = "https://www.powershellgallery.com/packages/$Name"
+    $request = [System.Net.WebRequest]::Create($url)
+    # do not allow to redirect. The result is a "MovedPermanently"
+    $request.AllowAutoRedirect = $false
+    try {
+        # send the request
+        $response = $request.GetResponse()
+        # get back the URL of the true destination page, and split off the version
+        $response.GetResponseHeader("Location").Split("/")[-1]
+        # make sure to clean up
+        $response.Close()
+        $response.Dispose()
+    }
+    catch {
+        Write-Error $_.Exception.Message
+    }
 }
 
 Write-Output "Querying Automation Account Name and Resource Group"
@@ -94,10 +98,27 @@ catch {
 Write-Output ("Importing module")
 if (Get-Command "New-AzAutomationModule" -ErrorAction SilentlyContinue) {
     New-AzAutomationModule -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroupName -Name $moduleName -ContentLink "https://www.powershellgallery.com/api/v2/package/$moduleName/$moduleVersion"
-} elseif (Get-Command "New-AzureRMAutomationModule" -ErrorAction SilentlyContinue) {
+}
+elseif (Get-Command "New-AzureRMAutomationModule" -ErrorAction SilentlyContinue) {
     New-AzureRMAutomationModule -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroupName -Name $moduleName -ContentLink "https://www.powershellgallery.com/api/v2/package/$moduleName/$moduleVersion"
 }
 else {
     $ErrorMessage = "No automation management module found"
     throw $ErrorMessage
 }
+
+
+if ($waitForDeployment) {
+    Write-Output ("Waiting for module deployment to complete")
+    $moduleState = ""
+    while ($moduleState -ne "Succeeded") {
+        $moduleState = (Get-AzAutomationModule -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroupName -Name $moduleName).ProvisioningState
+        if ($moduleState -eq "Failed") {
+            throw "Module deployment failed"
+        }
+        Write-Output ("..")
+        Start-Sleep 10
+    }
+    Write-Output ("Importing module succeeded")
+}
+
