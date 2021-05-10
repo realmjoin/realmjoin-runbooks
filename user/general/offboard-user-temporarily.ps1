eff612a60b4bfca6ee23038391202bf28d977238
Hans-Carl Oberdalhoff <Hans-Carl.Oberdalhoff@glueckkanja-gab.com>
2021-05-10 15:45:51 +0200

    initial release of "offboard user temp."

--- user/general/offboard-user-temporarily.ps1
+++ user/general/offboard-user-temporarily.ps1
@@ -1,17 +1,20 @@
 # This runbook is intended to orchestrate the different steps to temporarily offboard a user. This can be cases like parental leaves or sabaticals. 
 
-# If you store a users group memberships, you will need to have access
-
 #Requires -Modules AzureAD, RealmJoin.RunbookHelper, Az.Storage
 
 param (
-    [String] $UserName
+    [Parameter(Mandatory = $true)]
+    [String] $UserName,
+    [Parameter(Mandatory = $true)]
+    [String] $OrganizationID
 )
 
+Write-Output "Getting Process configuration URL"
+$processConfigURL = Get-AutomationVariable -name "SettingsSourceUserLeaverTemporary" -ErrorAction Stop
+Write-Output "Process Config URL is $($processConfigURL)"
 Write-Output "Getting Process configuration"
-$processConfigURL = Get-AutomationVariable -name "SettingsSourceUserLeaverTemporary" -ErrorAction SilentlyContinue
-$webResult = Invoke-WebRequest -UseBasicParsing -Uri $processConfigURL
-$processConfig = $webResult.Content | ConvertFrom-Json    
+$webResult = Invoke-WebRequest -UseBasicParsing -Uri $processConfigURL -ErrorAction Stop
+$processConfig = $webResult.Content | ConvertFrom-Json
 
 #region Authentication
 $connectionName = "AzureRunAsConnection"
@@ -30,7 +33,7 @@ catch {
 #endregion
 
 #region Get User object
-write-output ("Find the user object " + $UserName) 
+write-output ("Finding the user object " + $UserName) 
 $targetUser = Get-AzureADUser -ObjectId $UserName -ErrorAction SilentlyContinue
 if ($null -eq $targetUser) {
     throw ("User " + $UserName + " not found.")
@@ -39,52 +42,78 @@ if ($null -eq $targetUser) {
 
 #region Disable user
 if ($processConfig.disableUser) {
-    Write-Output "Block user sign in"
+    Write-Output "Blocking user sign in for $UserName"
     Set-AzureADUser -ObjectId $targetUser.ObjectId -AccountEnabled $false
 }
 #endregion
 
 #region Export / Backup group and DL memberships
-# Get list of group and role memberships. Write to file, as Set-AzStorageBlobContent needs a file to upload.
-Get-AzureADUserMembership -ObjectId $UserName -All $true > "memberships.txt"
-# Connect to / create Azure Storage Account
+write-output "Getting list of group and role memberships for user $UserName." 
+# Write to file, as Set-AzStorageBlobContent needs a file to upload.
+$memberships = Get-AzureADUserMembership -ObjectId $targetUser.ObjectId -All $true
+$memberships | Select-Object -Property "DisplayName","ObjectId" | ConvertTo-Json > memberships.txt
+#Write-Output "Connectint to Azure Storage Account"
 if ($processConfig.exportGroupMemberships) {
+    #FIXME - Which subscription to use? Variable?
+    Connect-AzAccount `
+            -ServicePrincipal `
+            -Tenant $servicePrincipalConnection.TenantId `
+            -ApplicationId $servicePrincipalConnection.ApplicationId `
+            -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint | Out-Null
     $AzAAResourceGroup = Get-AutomationVariable -name "AzAAResourceGroup" -ErrorAction Stop
     $storAccount = Get-AzStorageAccount -ResourceGroupName $AzAAResourceGroup -Name $processConfig.exportStorAccountName -ErrorAction SilentlyContinue
     if (-not $storAccount) {
-        $storAccount = New-AzStorageAccount -ResourceGroupName $AzAAResourceGroup -Name $processConfig.exportStorAccountName -ErrorAction Stop
+        Write-Output "Creating Azure Storage Account $($processConfig.exportStorAccountName)"
+        ##FIXME: convert to variable? Needs to be unique per env. 
+        $storAccount = New-AzStorageAccount -ResourceGroupName $AzAAResourceGroup -Name $processConfig.exportStorAccountName -Location $processConfig.exportStorAccountLocation -SkuName $processConfig.exportStorAccountSKU -ErrorAction Stop
     }
     $keys = Get-AzStorageAccountKey -ResourceGroupName $AzAAResourceGroup -Name $processConfig.exportStorAccountName
     $context = New-AzStorageContext -StorageAccountName $processConfig.exportStorAccountName -StorageAccountKey $keys[0].Value
-    $container = Get-AzStorageContainer -Name $processConfig.exportStorContainerGroupmembershipExports -Context $context -ErrorAction SilentlyContinue
+    $container = Get-AzStorageContainer -Name $processConfig.exportStorContainerGroupMembershipExports -Context $context -ErrorAction SilentlyContinue
     if (-not $container) {
+        Write-Output "Creating Azure Storage Account Container $($processConfig.exportStorContainerGroupmembershipExports)"
         $container = New-AzStorageContainer -Name $processConfig.exportStorContainerGroupmembershipExports -Context $context -ErrorAction Stop
     }
 }
-# Upload list. This might overwrite older versions.
-Set-AzStorageBlobContent -File "memberships.txt" -Container $container -Blob $UserName -Context $context -Force
+Write-Output "Uploading list of memberships. This might overwrite older versions."
+Set-AzStorageBlobContent -File "memberships.txt" -Container $processConfig.exportStorContainerGroupmembershipExports -Blob $UserName -Context $context -Force -ErrorAction Stop
+Disconnect-AzAccount -Confirm:$false
 #endregion
 
 #region Change license (group membership)
 if ($processConfig.changeLicenses) {
+    # Add new licensing group, if not already assigned
+    $processConfig.licenseGroupsToAdd | ForEach-Object {
+        $group =  Get-AzureADGroup -Filter "DisplayName eq `'$_`'" 
+        Write-Output "Adding License group $_ to user $UserName"
+        Add-AzureADGroupMember -RefObjectId $targetUser.ObjectID -ObjectId $group.ObjectID -ErrorAction Continue
+    }
 
+    # Remove all other known licensing groups
+    $groups = Get-AzureADUserMembership -ObjectId $targetUser.ObjectId
+    $groups | Where-Object { $_.DisplayName.startswith($processConfig.licenseGroupsToRemovePrefix)} | ForEach-Object {
+        if (-not $processConfig.licenseGroupsToAdd.contains($_.DisplayName)) {
+            Write-Output "Removing license group $($_.DisplayName) from $UserName"
+            Remove-AzureADGroupMember -MemberId $targetUser.ObjectId -ObjectId $_.ObjectID -ErrorAction Continue
+        }
+    }
 }
 #endregion
 
 #region grant other user access to mailbox
 if ($processConfig.grantAccessToMailbox) {
-
+##TODO
 }
 #endregion
 
 #region hide mailbox in adresslist
 if ($processConfig.hideFromAddresslist) {
-
+##TODO
 }
 
 #region out of office message
 if ($processConfig.setOutOfOffice) {
-
+##TODO
 }
 #endregion
 
@@ -94,17 +123,20 @@ if ($processConfig.setOutOfOffice) {
 
 #region remove MFA methods
 if ($processConfig.removeMFAMethods) {
-    
+##TODO    
 }
 #endregion
 
 #region remove other (security) groups
+##TODO
 #endregion
 
 #region de-associate client
+##TODO
 #endregion
 
 #region wipe client
+##TODO
 #endregion
 
 #region finishing
