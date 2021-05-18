752ef95674871c857553d1e86e54e8b3914b89a9
Hans-Carl Oberdalhoff <Hans-Carl.Oberdalhoff@glueckkanja-gab.com>
2021-05-18 18:36:25 +0200

    updated "(un)assign license"

--- user/general/unassign-license.ps1
+++ user/general/unassign-license.ps1
@@ -1,6 +1,10 @@
 # This runbook will remove a license from a user via removing a group membership.
 #
 # This runbook will use the "AzureRunAsConnection" to connect to AzureAD. Please make sure, enough API-permissions are given to this service principal.
+#
+# Permissions:
+#  AzureAD Roles
+#   - User administrator
 
 #Requires -Module MEMPSToolkit, RealmJoin.RunbookHelper
 
@@ -8,12 +12,13 @@ param(
     [Parameter(Mandatory = $true)]
     [String] $UserName,
     [Parameter(Mandatory = $true)]
-    ## FIXME Picker not working!
-    #[ValidateScript( { Use-RJInterface -Type Graph -Entity Group } )]
-    [String] $UI_GroupID_License,
+    [ValidateScript( { Use-RJInterface -Type Graph -Entity Group } )]
+    [String] $GroupID_License,
     [Parameter(Mandatory = $true)]
     [String] $OrganizationID
 )
+
+$ErrorActionPreference = "Stop"
 
 #region module check
 $neededModule = "MEMPSToolkit"
@@ -23,41 +28,43 @@ if (-not (Get-Module -ListAvailable $neededModule)) {
 }
 else {
     Import-Module $neededModule
-    Write-Output ("Module " + $neededModule + " is available.")
+    # "Module " + $neededModule + " is available."
 }
+#endregion
+
+#region authentication
+# Automation credentials
+$automationCredsName = "realmjoin-automation-cred"
+
+# "Connect to Graph API..."
+$token = Get-AzAutomationCredLoginToken -tenant $OrganizationID -automationCredName $automationCredsName
 #endregion
 
 # Licensing group prefix
 $groupPrefix = "LIC_"
 
-# Automation credentials
-$automationCredsName = "realmjoin-automation-cred"
-
-Write-Output "Connect to Graph API..."
-$token = Get-AzAutomationCredLoginToken -tenant $OrganizationID -automationCredName $automationCredsName
-
-write-output ("Find selected group from Object ID " + $UI_GroupID_License)
-$group = Get-AADGroupById -groupId $UI_GroupID_License -authToken $token
+# "Find select group from Object ID " + $GroupID_License
+$group = Get-AADGroupById -groupId $GroupID_License -authToken $token
 if (-not $group.displayName.startswith($groupPrefix)) {
-    throw "Please select a licensing group."
+    throw "`'$($group.displayName)`' is not a license assignment group. Will not proceed."
 }
 
-write-output ("Find the user object " + $UserName) 
+# "Find the user object " + $UserName) 
 $targetUser = get-AADUserByUPN -userName $UserName -authToken $token
 if ($null -eq $targetUser) {
-    throw ("User " + $UserName + " not found.")
+    throw ("User $UserName not found.")
 }
 
-write-output ("Is user member of the the group?")
-$members = Get-AADGroupMembers -groupID $UI_GroupID_License -authToken $token
+# "Is user member of the the group?"
+$members = Get-AADGroupMembers -groupID $GroupID_License -authToken $token
 if ($members.id -contains $targetUser.id) {
-    Write-Output "Removing license."
-    Remove-AADGroupMember -groupID $UI_GroupID_License -userID $targetUser.id -authToken $token
+    "Removing license."
+    Remove-AADGroupMember -groupID $GroupID_License -userID $targetUser.id -authToken $token | Out-Null
 }
 else {
-    Write-Output "License is not assigned. Doing nothing."
+    "License is not assigned. Doing nothing."
     
 }
 
-Write-Output ($group.displayName + " is unassigned from " + $UserName)
+"`'$($group.displayName)`' is unassigned from `'$UserName`'"
 
