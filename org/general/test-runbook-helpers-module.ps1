1afa6c911ae1d40e098f1c48e98203d41ccb74d2
Felix Storm <felix.storm@glueckkanja.com>
2021-05-26 17:08:40 +0200

    add org/general/test-runbook-helpers-module.ps1

---
+++ org/general/test-runbook-helpers-module.ps1
@@ -0,0 +1,13 @@
+#Requires -Module @{ModuleName = "RealmJoin.RunbookHelper"; ModuleVersion = "0.4.0" }, Az.Accounts, Az.Resources, AzureAD, ExchangeOnlineManagement
+
+Connect-RjRbAzAccount
+Get-AzADUser | Format-Table | Out-String
+
+Connect-RjRbAzureAD
+Get-AzureADUser | Format-Table | Out-String
+
+Connect-RjRbExchangeOnline
+Get-EXOMailbox | Format-Table | Out-String
+
+Connect-RjRbGraph
+Invoke-RjRbRestMethodGraph "/users" -OdSelect displayName | Format-Table | Out-String
