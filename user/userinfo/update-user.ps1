446a515c8419e25bc68f8d98fb5c224c71a202bc
Hans-Carl Oberdalhoff <Hans-Carl.Oberdalhoff@glueckkanja-gab.com>
2021-05-26 19:08:13 +0200

    merging from dev/felixstorm

--- user/userinfo/update-user.ps1
+++ user/userinfo/update-user.ps1
@@ -1,10 +1,10 @@
 # This runbook will update fields of an existing user object.
 
-# Requires #Requires -Module AzureAD, RealmJoin.RunbookHelper
+#Requires -Module @{ModuleName = "RealmJoin.RunbookHelper"; ModuleVersion = "0.4.0" }, AzureAD
 
 param (
     [Parameter(Mandatory = $true)]
-    [String]$UserName,
+    [string]$UserName,
     [string]$givenName,
     [string]$surname,
     [string]$displayName,
@@ -23,214 +23,48 @@ param (
 
 )
 
-#region Module check
-function Test-ModulePresent {
-    param(
-        [Parameter(Mandatory = $true)]
-        [string]$neededModule
-    )
-    if (-not (Get-Module -ListAvailable $neededModule)) {
-        throw ($neededModule + " is not available and can not be installed automatically. Please check.")
-    }
-    else {
-        Import-Module $neededModule
-        # "Module " + $neededModule + " is available."
-    }
-}
-
-Test-ModulePresent "AzureAD"
-Test-ModulePresent "RealmJoin.RunbookHelper"
-#endregion
-
-#region Authentication
-# "Connecting to AzureAD"
 Connect-RjRbAzureAD
-#endregion
 
-# Bug in AzureAD Module when using ErrorAction
-$ErrorActionPreference = "SilentlyContinue"
-
-# "Searching for user $UserName."
-$targetUser = Get-AzureADUser -ObjectId $UserName 
-if (-not $targetUser) {
-    throw ("User $UserName not found.")
-}
-
-
-# Bug in AzureAD Module when using ErrorAction
-$ErrorActionPreference = "Stop"
-
-#region atomic changes
-if ($givenName) {
-    try {
-        "Setting givenName to `"$givenName`""
-        Set-AzureADUser -ObjectId $targetUser.ObjectId -GivenName $givenName 
-    }
-    catch {
-        throw "Updating user object failed!"
-    }
-}
-
-if ($surname) {
-    try {
-        "Setting surname to `"$surname`""
-        Set-AzureADUser -ObjectId $targetUser.ObjectId -Surname $surname
-    }
-    catch {
-        throw "Updating user object failed!"
-    }
-}
-
-if ($displayName) {
-    try {
-        "Setting displayName to `"$displayName`""
-        Set-AzureADUser -ObjectId $targetUser.ObjectId -DisplayName $displayName
-    }
-    catch {
-        throw "Updating user object failed!"
-    }
-}
-
-if ($companyName) {
-    try {
-        "Setting companyName to `"$companyName`""
-        Set-AzureADUser -ObjectId $targetUser.ObjectId -CompanyName $companyName
-    }
-    catch {
-        throw "Updating user object failed!"
-    }
-}
-
-if ($city) {
-    try {
-        "Setting city to `"$city`""
-        Set-AzureADUser -ObjectId $targetUser.ObjectId -City $city
-    }
-    catch {
-        throw "Updating user object failed!"
-    }
-}
-
-if ($country) {
-    try {
-        "Setting country to `"$country`""
-        Set-AzureADUser -ObjectId $targetUser.ObjectId -Country $country
-    }
-    catch {
-        throw "Updating user object failed!"
-    }
-}
-
-if ($jobTitle) {
-    try {
-        "Setting jobTitle to `"$jobTitle`""
-        Set-AzureADUser -ObjectId $targetUser.ObjectId -JobTitle $jobTitle
-    }
-    catch {
-        throw "Updating user object failed!"
-    }
-}
-
-# Be aware - naming difference local AD and AzureAD!
-if ($officeLocation) {
-    try {
-        "Setting officeLocation to `"$officeLocation`""
-        Set-AzureADUser -ObjectId $targetUser.ObjectId -PhysicalDeliveryOfficeName $officeLocation
-    }
-    catch {
-        throw "Updating user object failed!"
-    }
-}
-
-if ($postalCode) {
-    try {
-        "Setting postalCode to `"$postalCode`""
-        Set-AzureADUser -ObjectId $targetUser.ObjectId -PostalCode $postalCode
-    }
-    catch {
-        throw "Updating user object failed!"
-    }   
-}
-
-if ($preferredLanguage) {
-    try {
-        "Setting preferredLanguage to `"$preferredLanguage`""
-        Set-AzureADUser -ObjectId $targetUser.ObjectId -PreferredLanguage $preferredLanguage
-    }
-    catch {
-        throw "Updating user object failed!"
-    }   
-}
-
-if ($state) {
-    try {
-        "Setting state to `"$state`""
-        Set-AzureADUser -ObjectId $targetUser.ObjectId -State $state
-    }
-    catch {
-        throw "Updating user object failed!"
-    }   
-}
-
-if ($streetAddress) {
-    try {
-        "Setting streetAddress to `"$streetAddress`""
-        Set-AzureADUser -ObjectId $targetUser.ObjectId -StreetAddress $streetAddress
-    }
-    catch {
-        throw "Updating user object failed!"
-    }   
-}
-
-if ($usageLocation) {
-    try {
-        "Setting usageLocation to `"$usageLocation`""
-        Set-AzureADUser -ObjectId $targetUser.ObjectId -UsageLocation $usageLocation
-    }
-    catch {
-        throw "Updating user object failed!"
-    }   
-}
-#endregion
-
-#load new state from AzureAD
+Write-RjRbLog "Searching for user '$UserName'"
 $targetUser = Get-AzureADUser -ObjectId $UserName
 
-#region inference updates
-if ((-not $targetUser.DisplayName) -and ($targetUser.GivenName) -and ($targetUser.Surname)) {
-    $displayName = ($targetUser.GivenName + " " + $targetUser.Surname)
-    try {
-        "Setting displayName to `"$displayName`""
-        Set-AzureADUser -ObjectId $targetUser.ObjectId -DisplayName $displayName
-    }
-    catch {
-        throw "Updating user object failed!"
-    }
-    
-} elseif (-not $targetUser.DisplayName) {
-    $displayName = $targetUser.MailNickName
-    try {
-        "Setting displayName to `"$displayName`""
-        Set-AzureADUser -ObjectId $targetUser.ObjectId -DisplayName $displayName
-    }
-    catch {
-        throw "Updating user object failed!"
+$userArgs = @{}
+function addToUserArgs($variableName, $paramName = $variableName) {
+    $paramValue = Get-Variable $variableName -ValueOnly -EA 0
+    if ($paramValue) {
+        $userArgs[$paramName] = $paramValue
     }
 }
+addToUserArgs 'givenName'
+addToUserArgs 'surname'
+addToUserArgs 'displayName'
+addToUserArgs 'companyName'
+addToUserArgs 'city'
+addToUserArgs 'country'
+addToUserArgs 'jobTitle'
+addToUserArgs 'officeLocation' 'PhysicalDeliveryOfficeName'
+addToUserArgs 'postalCode'
+addToUserArgs 'preferredLanguage'
+addToUserArgs 'state'
+addToUserArgs 'streetAddress'
+addToUserArgs 'usageLocation'
 
-if (-not $targetUser.CompanyName) {
-    $companyName = (Get-AzureADTenantDetail).DisplayName
-    try {
-        "Setting companyName to `"$companyName`""
-        Set-AzureADUser -ObjectId $targetUser.ObjectId -CompanyName $companyName
+if (-not $targetUser.DisplayName -and -not $displayName) {
+    $resultingGivenName = ($givenName, $targetUser.GivenName -ne "" -ne $null)[0]
+    $resultingSurname = ($surname, $targetUser.Surname  -ne "" -ne $null)[0]
+    if ($resultingGivenName -and $resultingSurname) {
+        $userArgs['displayName'] = "$resultingGivenName $resultingSurname"
     }
-    catch {
-        throw "Updating user object failed!"
+    else {
+        $userArgs['displayName'] = $targetUser.MailNickName
     }
 }
-#endregion
+if (-not $targetUser.CompanyName -and -not $companyName) {
+    $userArgs['companyName'] = (Get-RjRbAzureADTenantDetail).DisplayName
+}
 
-"Disconnecting from AzureAD."
-Disconnect-AzureAD
+Write-RjRbLog "Updating user object with the following properties" $userArgs
+Set-AzureADUser -ObjectId $targetUser.ObjectId @userArgs
 
-"User $UserName successfully updated."
+
+"User '$UserName' successfully updated."
