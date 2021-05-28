1a722a6ac8bda4176f02de94eef4db7f61abbf19
Hans-Carl Oberdalhoff <Hans-Carl.Oberdalhoff@glueckkanja-gab.com>
2021-05-28 14:49:56 +0200

    v1 - "new room mailbox"

---
+++ org/mail/add-room-mailbox.ps1
@@ -0,0 +1,48 @@
+#Requires -Module @{ModuleName = "RealmJoin.RunbookHelper"; ModuleVersion = "0.4.0" }, ExchangeOnlineManagement
+
+param (
+    [Parameter(Mandatory = $true)] [string] $mailboxName,
+    [string] $displayName,
+    [ValidateScript( { Use-RJInterface -Type Graph -Entity User } )]
+    [string] $delegateTo,
+    [int] $capacity,
+    [bool] $autoAccept = $false
+)
+
+$VerbosePreference = "SilentlyContinue"
+
+Connect-RjRbExchangeOnline
+
+$invokeParams = @{
+    Name = $mailboxName
+    Alias = $mailboxName
+    Room = $true
+}
+
+if ($displayName) {
+    $invokeParams += @{ DisplayName = $displayName }
+}
+
+if ($capacity -and ($capacity -gt 0)){
+    $invokeParams += @{ ResourceCapacity = $capacity }
+}
+
+# Create the mailbox
+$mailbox = New-Mailbox @invokeParams
+
+if ($delegateTo) {
+    # "Grant SendOnBehalf"
+    $mailbox | Set-Mailbox -GrantSendOnBehalfTo $delegateTo | Out-Null
+    # "Grant FullAccess"
+    $mailbox | Add-MailboxPermission -User $delegateTo -AccessRights FullAccess -InheritanceType All -AutoMapping $AutoMapping -confirm:$false | Out-Null
+    # Calendar delegation
+    Set-CalendarProcessing -Identity $mailboxName -ResourceDelegates $delegateTo 
+}
+
+if ($autoAccept) {
+    Set-CalendarProcessing -Identity $mailboxName -AutomateProcessing "AutoAccept"
+}
+
+"Room Mailbox $mailboxName has been created."
+
+Disconnect-ExchangeOnline -Confirm:$false -ErrorAction SilentlyContinue | Out-Null
