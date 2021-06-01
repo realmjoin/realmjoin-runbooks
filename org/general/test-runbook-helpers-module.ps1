1b9b754e29e0090a6f4b8136e22bf6573bd3fc1c
Felix Storm <felix.storm@glueckkanja.com>
2021-06-01 17:46:30 +0200

    update test runbooks

--- org/general/test-runbook-helpers-module.ps1
+++ org/general/test-runbook-helpers-module.ps1
@@ -1,4 +1,4 @@
-#Requires -Module @{ModuleName = "RealmJoin.RunbookHelper"; ModuleVersion = "0.4.0" }, Az.Accounts, Az.Resources, AzureAD, ExchangeOnlineManagement
+#Requires -Module @{ModuleName = "RealmJoin.RunbookHelper"; ModuleVersion = "0.5.0" }, Az.Accounts, Az.Resources, AzureAD, ExchangeOnlineManagement
 
 Connect-RjRbAzAccount
 Get-AzADUser | Format-Table | Out-String
@@ -8,7 +8,8 @@ Get-AzureADUser | Format-Table | Out-String
 
 Connect-RjRbExchangeOnline
 Get-EXOMailbox | Format-Table | Out-String
-Disconnect-ExchangeOnline
+Disconnect-ExchangeOnline -Confirm:$false -ErrorAction Continue
 
 Connect-RjRbGraph
 Invoke-RjRbRestMethodGraph "/users" -OdSelect displayName | Format-Table | Out-String
+Invoke-RjRbRestMethodGraph "/users/91ae5a52-67f6-4265-bbe5-b62268944675" | Format-List | Out-String
