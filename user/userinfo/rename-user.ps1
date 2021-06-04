1edde4281ebe8f416fe623e3f26246eab35362b4
Hans-Carl Oberdalhoff <Hans-Carl.Oberdalhoff@glueckkanja-gab.com>
2021-06-04 13:14:47 +0200

    redone logic flow - less edge cases

--- user/userinfo/rename-user.ps1
+++ user/userinfo/rename-user.ps1
@@ -1,10 +1,11 @@
-# This rename a user object
+# This rename a user object. This will not update the DisplayName, GivenName, Surname.
+#
+# Currently, removing the old eMail-address "in one go" seems not to work reliably
+# Updating the SIP takes some minutes tp propagate
 #
 # Permissions: MS Graph API
 # - Directory.Read.All
 # - User.ReadWrite.All
-
-## TODO: Change IM Alias?
 
 #Permissons -MSGraph Directory.Read.All:Api, User.ReadWrite.All:Api -ExchangeOnline Exchange.ManageAsApp:Api -AzureADRole "Application administrator"
 
@@ -16,7 +17,8 @@ param(
     [Parameter(Mandatory = $true)] [string] $UserId,
     [Parameter(Mandatory = $true)] [string] $NewUpn,
     [bool] $ChangeMailnickname = $true,
-    [bool] $RemoveOldEmail = $false
+    [bool] $UpdatePrimaryAddress = $true
+    # [bool] $RemoveOldAddress = $false
 )
 
 Connect-RjRbGraph
@@ -25,36 +27,47 @@ Connect-RjRbExchangeOnline
 # Get original UPN
 $userObject = Invoke-RjRbRestMethodGraph -resource "/users/$UserId" 
 
-## Check if desired name is available
-## Skip for now - rename will fail gracefully
-#$otherUser = Invoke-RjRbRestMethodGraph -resource "/users/$NewUpn" -ErrorAction SilentlyContinue
-#if ($otherUser) {
-#    throw "Username '$NewUpn' is already taken"
-#}
-
+# Change UPN
 $body = @{
     userPrincipalName = $NewUpn
 }
-
-# Change UPN
 Invoke-RjRbRestMethodGraph -resource "/users/$UserId" -Method Patch -Body $body
 
-# Get User / Mailbox
+# Change eMail-Adresses
 $mailbox = Get-EXOMailbox -Identity $UserId -ErrorAction SilentlyContinue
 if ($mailbox) {
 
     if ($ChangeMailnickname) {
-        Set-Mailbox -Identity $mailbox.Identity -Name $NewUpn.split('@')[0] -alias $NewUpn.split('@')[0]
+        Set-Mailbox -Identity $UserId -Name $NewUpn.split('@')[0] -alias $NewUpn.split('@')[0] | Out-Null
     }
 
-    # Update
-    $mailbox = Get-EXOMailbox -Identity $UserId -ErrorAction SilentlyContinue
-    
-    if ($RemoveOldEmail) {
-        if ($mailbox.EmailAddresses -icontains "smtp:$($userObject.UserPrincipalName)") {
-            # Remove email address
-            Set-Mailbox -Identity $mailbox.Identity -EmailAddresses @{remove = "$($userObject.UserPrincipalName)" }
+    if ($UpdatePrimaryAddress) {
+
+        $newAdresses = New-Object System.Collections.ArrayList
+        $newAdresses.Add("SMTP:$NewUpn") | Out-Null
+
+        foreach ($address in $mailbox.EmailAddresses) {
+            $prefix = $address.Split(":")[0] 
+            $mail = $address.Split(":")[1]
+            
+            if ($prefix -eq "smtp") {
+                if ($mail -eq $NewUpn ) {
+                    # Do nothing - primary email-address is already added
+                }
+                #elseif (($mail -eq $($userObject.UserPrincipalName)) -and $RemoveOldAddress) {
+                    # Do nothing - do not add old email-address
+                #}
+                else {
+                    $newAdresses.Add($address.ToLower()) | Out-Null
+                }
+            }
+            else {
+                # not specifying the SIP address will automatically update it.
+                # $newAdresses.Add($address) | Out-Null
+            }
         }
+
+        Set-Mailbox -Identity $UserId -EmailAddresses $newAdresses
     }
 }
     
