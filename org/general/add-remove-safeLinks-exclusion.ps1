3fa8c687c7d859a62a9556329c90d2cc95effed7
Hans-Carl Oberdalhoff <Hans-Carl.Oberdalhoff@glueckkanja-gab.com>
2021-06-08 12:44:45 +0200

    v1 of "add/remove safelinks exclusion"

---
+++ org/general/add-remove-safeLinks-exclusion.ps1
@@ -0,0 +1,46 @@
+# This will add or remove a SafeLinks URL exclusion to/from a given policy.
+
+#Requires -Module @{ModuleName = "RealmJoin.RunbookHelper"; ModuleVersion = "0.5.0" }, ExchangeOnlineManagement
+
+param(
+    # Not mandatory to allow an example value
+    [String] $LinkPattern = "https://*.microsoft.com/*",
+    # If only one policy exists, no need to specify
+    [String] $PolicyName,
+    [boolean] $RemovePattern = $false
+
+)
+
+Connect-RjRbExchangeOnline
+
+$policy = $null
+if ($PolicyName) {
+    # "Use the given policy $PolicyName"
+    $policy = Get-SafeLinksPolicy -Identity $PolicyName -ErrorAction SilentlyContinue
+} else {
+    # If there is only one policy - use it
+    $policies = [array](Get-SafeLinksPolicy -ErrorAction SilentlyContinue)
+    if (($policies).count -eq 1) {
+        $policy = $policies[0]
+    }
+}
+if (-not $policy) {
+    throw "Please give a SafeLinks Policy."
+}
+
+$DoNotRewriteUrls = @()
+if ($RemovePattern) {
+    foreach ($entry in $policy.DoNotRewriteUrls) {
+        if ($entry -ne $LinkPattern) {
+            $DoNotRewriteUrls += $entry
+        } 
+    }
+} else {
+    $DoNotRewriteUrls = $policy.DoNotRewriteUrls + $LinkPattern | Sort-Object -Unique
+}
+
+Set-SafeLinksPolicy -Identity $policy.Id -DoNotRewriteUrls $DoNotRewriteUrls | Out-Null
+
+Disconnect-ExchangeOnline -confirm:$false | Out-Null
+
+"Policy $PolicyName successfully updated."
