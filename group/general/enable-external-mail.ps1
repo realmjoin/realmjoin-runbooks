6e0f84cec039e8a88bc1de7446f78cdf177c5398
Hans-Carl Oberdalhoff <Hans-Carl.Oberdalhoff@glueckkanja-gab.com>
2021-06-14 10:51:54 +0200

    switched to Connect-RjRbExchangeOnline

--- group/general/enable-external-mail.ps1
+++ group/general/enable-external-mail.ps1
@@ -7,80 +7,49 @@
 # Azure AD Roles
 #  - Exchange administrator
 
-#Requires -Module ExchangeOnlineManagement, @{ModuleName = "RealmJoin.RunbookHelper"; ModuleVersion = "0.4.0" }
+#Requires -Module ExchangeOnlineManagement, @{ModuleName = "RealmJoin.RunbookHelper"; ModuleVersion = "0.5.1" }
 
 param
 (
     [Parameter(Mandatory = $true)]
     [ValidateScript( { Use-RJInterface -Type Graph -Entity Group } )]
     [String] $GroupName,
-    [boolean] $Disable,
-    [Parameter(Mandatory = $true)]
-    [String] $OrganizationInitialDomainName
+    [boolean] $Disable
 )
 
-Set-StrictMode -Version Latest
-$ErrorActionPreference = "Stop"
-$ProgressPreference = "SilentlyContinue"
-
-#region module check
-function Test-ModulePresent {
-    param(
-        [Parameter(Mandatory = $true)]
-        [string]$neededModule
-    )
-    if (-not (Get-Module -ListAvailable $neededModule)) {
-        throw ($neededModule + " is not available and can not be installed automatically. Please check.")
-    }
-    else {
-        Import-Module $neededModule
-        # "Module " + $neededModule + " is available."
-    }
-}
-
-Test-ModulePresent "ExchangeOnlineManagement"
-#endregion
-
-#region authentication
-$Connection = Get-AutomationConnection -Name 'AzureRunAsConnection'
-# "Connecting to Exchange online."
 try {
-    Connect-ExchangeOnline -CertificateThumbprint $Connection.CertificateThumbprint -AppId $Connection.ApplicationId -Organization $OrganizationInitialDomainName | Out-Null
-}
-catch {
-    throw "Connection to Exchange Online failed! `r`n $_"
-}
-# "Connection to Exchange Online Powershell established!"  
-#endregion
+    $ProgressPreference = "SilentlyContinue"
 
-# "Checking"  if group is universal group
-if (-not (Get-UnifiedGroup -Identity $GroupName -ErrorAction SilentlyContinue)) {
-    throw "`'$GroupName`' is not a unified (O365) group. Can not proceed."
-}
-    
-if ($Disable) {
-    # "Disable external mailing for $GroupName"
-    try {
-        Set-UnifiedGroup -Identity $GroupName -RequireSenderAuthenticationEnabled $true
-    }
-    catch {
-        throw "Couldn't disable external mailing! `r`n $_"
-    }
-    "External mailing successfully disabled for `'$GroupName`'"
-}
-    
-else {
-    # "Enabling external mailing for $GroupName"
-    try {
-        Set-UnifiedGroup -Identity $GroupName -RequireSenderAuthenticationEnabled $false
-    }
-    catch {
-        throw "Couldn't enable external mailing! `r`n $_"
-    }
-    "External mailing successfully enabled for `'$GroupName`'"
-}
+    Connect-RjRbExchangeOnline
 
-# "Disconnecting from EXO"
-Get-PsSession | Where-Object { $_.ConfigurationName -eq 'Microsoft.Exchange' } | Remove-PsSession | Out-Null
-Disconnect-ExchangeOnline -Confirm:$false | Out-Null
+    # "Checking"  if group is universal group
+    if (-not (Get-UnifiedGroup -Identity $GroupName -ErrorAction SilentlyContinue)) {
+        throw "`'$GroupName`' is not a unified (O365) group. Can not proceed."
+    }
+    
+    if ($Disable) {
+        # "Disable external mailing for $GroupName"
+        try {
+            Set-UnifiedGroup -Identity $GroupName -RequireSenderAuthenticationEnabled $true
+        }
+        catch {
+            throw "Couldn't disable external mailing! `r`n $_"
+        }
+        "External mailing successfully disabled for `'$GroupName`'"
+    }
+    
+    else {
+        # "Enabling external mailing for $GroupName"
+        try {
+            Set-UnifiedGroup -Identity $GroupName -RequireSenderAuthenticationEnabled $false
+        }
+        catch {
+            throw "Couldn't enable external mailing! `r`n $_"
+        }
+        "External mailing successfully enabled for `'$GroupName`'"
+    }
+}
+finally {
+    Disconnect-ExchangeOnline -Confirm:$false | Out-Null
+}
 
