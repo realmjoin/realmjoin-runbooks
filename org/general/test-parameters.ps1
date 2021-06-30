81db14081b71235fcbcd5a8ccf41bc5d5cfb1011
Felix Storm <felix.storm@glueckkanja.com>
2021-06-30 18:21:58 +0200

    add org/general/test-parameters.ps1

---
+++ org/general/test-parameters.ps1
@@ -0,0 +1,16 @@
+#Requires -Module @{ModuleName = "RealmJoin.RunbookHelper"; ModuleVersion = "0.5.1" }
+
+param
+(
+    [string] $TestString1,
+    [string] $TestString2,
+    [string] $TestString3,
+    [int] $TestInt,
+    [double] $TestDouble,
+    [bool] $TestBool,
+    [switch] $TestSwitch,
+    [object] $TestObject1,
+    [object] $TestObject2
+)
+
+$PSBoundParameters | ConvertTo-Json
