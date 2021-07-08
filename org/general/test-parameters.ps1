3a719f1b6808344c6f86ff0742c6a1c5ea464a05
Felix Storm <felix.storm@glueckkanja.com>
2021-07-08 18:19:58 +0200

    test-parameters.ps1

--- org/general/test-parameters.ps1
+++ org/general/test-parameters.ps1
@@ -1,23 +1,42 @@
+<#
+  .SYNOPSIS
+  Parameter testing only
+
+  .DESCRIPTION
+  Provides some tests on how different types of parameters are 
+  being passed to the PowerShell runbook scripts.
+#>
+
 #Requires -Module @{ModuleName = "RealmJoin.RunbookHelper"; ModuleVersion = "0.5.1" }
 
 [CmdletBinding(DefaultParameterSetName = 'Path')]
 param
 (
+    [string] $TestStringPlain,
+    [string] $TestStringWithDefault = "asdf",
+    [Parameter(Mandatory)]
+    [string] $TestStringMandatoryAndDefault = "asdf",
     [ValidateSet("Chocolate", "Strawberry", "Vanilla")]
-    [string] $TestString1,
-    [string] $TestString2,
-    [string] $TestString3,
+    [string] $TestStringValidateSet,
     [int] $TestInt,
     [double] $TestDouble,
-    [datetime] $TestDatetime,
+    [datetime] $TestDateTime,
+    [DateTimeOffset] $TestDateTimeOffset,
+    [ValidateScript( { Use-RJInterface -Date } )]
+    $TestDate,
+    [ValidateScript( { Use-RJInterface -Time } )]
+    $TestTime,
     [bool] $TestBool,
+    [bool] $TestBoolWithDefaultTrue = $true,
     [switch] $TestSwitch,
+    [switch] $TestSwitchWithDefaultPresent = [switch]::Present,
+    [switch] $TestSwitchWithDefaultTrue = $true,
     [object] $TestObject1,
-    [object] $TestObject2
+    [object] $TestObject2,
+    $TestNoType
 )
 
 $PSBoundParameters | ConvertTo-Json
 
-if ($TestDatetime) {
-    "`$TestDatetime: $($TestDatetime.ToString("o"))"
-}
+if ($TestDateTime) { "`$TestDatetime: $($TestDateTime.ToString("o")), $($TestDateTime.Kind)" }
+if ($TestDateTimeOffset) { "`$TestDateTimeOffset: $($TestDateTimeOffset.ToString("o")), $($TestDateTimeOffset.Offset)" }
