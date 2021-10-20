55faa9294f4f34ce6688ffdb06a6cd8aefcba644
Hans-Carl Oberdalhoff <hans-carl.oberdalhoff@primepulse.de>
2021-10-20 17:57:48 +0200

    new: - list policies - create new one if needed - use a default name if no pol. name is given - resolves #80

--- org/general/add-or-remove-safeLinks-exclusion.ps1
+++ org/general/add-or-remove-safeLinks-exclusion.ps1
@@ -4,12 +4,13 @@
 
   .DESCRIPTION
   Add or remove a SafeLinks URL exclusion to/from a given policy.
+  It can also be used to initially create a new policy if required.
 
   .PARAMETER LinkPattern
   URL to allow, can contain '*' as wildcard for host and paths
 
   .PARAMETER PolicyName
-  Optional if only one exists.
+  Optional, will overwrite default values
 
   .NOTES
   Permissions given to the Az Automation RunAs Account:
@@ -20,54 +21,238 @@
 
   .INPUTS
   RunbookCustomization: {
-        "Parameters": {
-            "Remove": {
-                "DisplayName": "Add or Remove URL Pattern to/from Policy",
-                "SelectSimple": {
-                    "Add URL Pattern to Policy": false,
-                    "Remove URL Pattern from Policy": true
-                }
+    "Parameters": {
+        "Action": {
+            "DisplayName": "Add or Remove URL Pattern to/from Policy",
+            "Select": {
+                "Options": 
+                [
+                    {
+                        "Display": "Add URL Pattern to Policy" ,
+                        "ParameterValue": 0
+                    },
+                    {
+                        "Display": "Remove URL Pattern from Policy",
+                        "ParameterValue": 1,
+                        "Customization": {
+                            "Hide": [
+                                "CreateNewPolicyIfNeeded"
+                            ]
+                        }
+                    },
+                    {
+                        "Display": "List all existing policies and settings",
+                        "ParameterValue": 2,
+                        "Customization": {
+                            "Hide": [
+                                "LinkPattern",
+                                "CreateNewPolicyIfNeeded",
+                                "PolicyName"
+                            ]
+                        }
+                    }
+                ]
             }
+        },
+        "DefaultPolicyName": {
+            "Hide": true
         }
     }
+}
 #>
 
 #Requires -Module @{ModuleName = "RealmJoin.RunbookHelper"; ModuleVersion = "0.6.0" }, ExchangeOnlineManagement
 
 param(
+    [ValidateScript( { Use-RJInterface -DisplayName "Action to execute" } )]
+    [int] $Action = 0,
     # Not mandatory to allow an example value
     [String] $LinkPattern = "https://*.microsoft.com/*",
-    # If only one policy exists, no need to specify
+    # If only one policy exists, no need to specify. Will use "DefaultPolicyName" as default otherwise.
+    [String] $DefaultPolicyName = "Default SafeLinks Policy",
+    # Using "PolicyName" will overwrite the defaults
     [ValidateScript( { Use-RJInterface -DisplayName "Safe Links policy name" } )]
     [String] $PolicyName,
-    [ValidateScript( { Use-RJInterface -DisplayName "Remove this pattern" } )]
-    [boolean] $Remove = $false
+    [boolean] $CreateNewPolicyIfNeeded = $true
 )
+
+function createGroupFromPolicyName {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string] $PolicyGroupName
+    )
+
+    # Check if group exists already
+    $mailNickName = "$($PolicyGroupName.replace(' ',''))Users"
+    $group = get-group -Identity $mailNickName -ErrorAction SilentlyContinue
+    if ($group) {
+        throw "Group '$mailNickName' already exists."
+    }
+
+    $groupDescription = @{
+        Name        = $mailNickName
+        DisplayName = "$($PolicyGroupName) Users"
+        Type        = "Security"
+        Notes       = "Add users to apply '$PolicyGroupName' to them"
+    }
+
+    $groupObj = New-DistributionGroup @groupDescription
+
+    return $groupObj
+    
+}
+
+function createSafeLinksPolicy {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string] $SafeLinksPolName,
+        [string] $GroupName = "$SafeLinksPolName Users"
+    )
+
+    $policyDescription = @{
+        Name                       = $SafeLinksPolName
+        AdminDisplayName           = "Default SafeLinks Policy - created by RealmJoin"
+        IsEnabled                  = $true
+        AllowClickThrough          = $false
+        DoNotAllowClickThrough     = $true
+        ScanUrls                   = $true 
+        EnableForInternalSenders   = $true
+        DeliverMessageAfterScan    = $true
+        EnableOrganizationBranding = $true
+        TrackClicks                = $true
+        DoNotTrackUserClicks       = $false
+        EnableSafeLinksForTeams    = $false
+        DisableUrlRewrite          = $false
+    }
+
+    $policy = New-SafeLinksPolicy @policyDescription
+    
+    # The Web Portal actually shows "rules" not the "policies" - so we name them identically
+    New-SafeLinksRule -name $SafeLinksPolName -SafeLinksPolicy $SafeLinksPolName -SentToMemberOf $GroupName | Out-Null
+
+    return $policy
+}
 
 try {
     Connect-RjRbExchangeOnline
 
+    if ($Action -eq 2) {
+        $policies = [array](Get-SafeLinksPolicy -ErrorAction SilentlyContinue)
+        "## Available Policies:"
+        $policies | ForEach-Object {
+            ""
+            "## Policy name: $($_.Name)"
+            $_ | Select-Object -Property IsEnabled,AllowClickThrough,DoNotAllowClickThrough,ScanUrls,EnableForInternalSenders,DeliverMessageAfterScan,EnableOrganizationBranding,TrackClicks,DoNotTrackUserClicks,EnableSafeLinksForTeams,DisableUrlRewrite,DoNotRewriteUrls | Format-List | Out-String
+        }
+        
+        exit
+    }
+    
     $policy = $null
+
+    # Work using a required Policy Name
     if ($PolicyName) {
         $policy = Get-SafeLinksPolicy -Identity $PolicyName -ErrorAction SilentlyContinue
-    }
-    else {
-        # If there is only one policy - use it
-        $policies = [array](Get-SafeLinksPolicy -ErrorAction SilentlyContinue)
-        if (($policies).count -eq 1) {
-            $policy = $policies[0]
+        
+        if (-not $policy) {
+            "## Policy '$PolicyName' not found."
+            # Remove...
+            if ($Action -eq 1) {
+                "## Exiting - nothing to do."
+                exit
+            }
+            else {
+                if ($CreateNewPolicyIfNeeded) {
+                    # Add
+                    "## Creating new policy '$PolicyName'"
+
+                    ""
+                    $groupObj = createGroupFromPolicyName -PolicyGroupName $PolicyName
+                    "## Created group '$($groupObj.Name)'"
+                    "## Add users to this group to apply SafeLinks to them."
+            
+                    ""
+                    "## Creating SafeLinks Policy from template"
+                    $policy = createSafeLinksPolicy -SafeLinksPolName $PolicyName -GroupName $groupObj.Name
+                }
+                else {
+                    "## Policy '$PolicyName' not found."
+                    ""
+                    throw("Policy not found")
+                }
+            }
+        }
+    } 
+
+    # Is our default policy available?
+    if ((-not $policy) -and $DefaultPolicyName) {
+        $policy = Get-SafeLinksPolicy -Identity $DefaultPolicyName -ErrorAction SilentlyContinue
+
+        if (-not $policy) {
+            "## Policy '$DefaultPolicyName' not found."
+
+            $policies = [array](Get-SafeLinksPolicy -ErrorAction SilentlyContinue)
+
+            # Do we have exactly one policy in this tenant?
+            if ($policies.count -eq 1) {
+                $policy = $policies[0]
+            }
+            # Multiple policies exist, but do not match
+            if ($policies.count -gt 1) {
+                "## Multiple policies available. Please select one."
+                ""
+                "## Available policies:"
+                $policies.Id
+                ""
+                throw ("Policy not selected")
+            }
+            # No policies available...
+            if (($policies).count -eq 0) {
+                "## No SafeLinks Policies exist/found."
+                if ($Action -eq 1) {
+                    # Remove
+                    "## Exiting - nothing to do."
+                    exit
+                }
+                else {
+                    # Add
+                    if ($CreateNewPolicyIfNeeded) {
+                        "## Creating new policy '$DefaultPolicyName'"
+
+                        ""
+                        $groupObj = createGroupFromPolicyName -PolicyGroupName $DefaultPolicyName
+                        "## Created group '$($groupObj.Name)'"
+                        "## Add users to this group to apply SafeLinks to them."
+            
+                        ""
+                        "## Creating SafeLinks Policy from template"
+                        $policy = createSafeLinksPolicy -SafeLinksPolName $DefaultPolicyName -GroupName $groupObj.Name
+                    }
+                    else {
+                        ""
+                        throw("Policy not found")
+                    }
+                }
+            }
         }
     }
+
+    # failsafe...
     if (-not $policy) {
-        throw "No policy found. Please provide a SafeLinks policy."
+        "## Policy not found. Please provide a SafeLinks policy."
+        ""
+        "## Available policies:"
+        $policies = [array](Get-SafeLinksPolicy -ErrorAction SilentlyContinue)
+        $policies.Id
+        ""
+        throw("Policy not found or created")
     }
 
-    "## Policy is defined as $policy"
-    $policy | Format-Table -Property Name,IsEnabled,IsDefault | Out-String
+    "## Using policy '$($policy.name)'"
 
     ""
-    "## Current list of excluded Safe Links"
-    $policy | Select-Object -expandproperty DoNotRewriteUrls
+    "## Current list of URLs excluded from Safe Links:"
+    $policy.DoNotRewriteUrls
 
     ""
     $DoNotRewriteUrls = @()
@@ -87,10 +272,11 @@ try {
     "## Policy $policy successfully updated."
 
     ""
-    "## Safe Link Policy Dump"
+    "## Safe Links Policy Dump"
     Get-SafeLinksPolicy -Identity $policy.Id | Format-List | Out-String
 
 }
+
 finally {
     Disconnect-ExchangeOnline -confirm:$false | Out-Null
 }
