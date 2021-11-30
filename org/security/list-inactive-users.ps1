059d346d772b47e295691f41741bcb4b0bb44c31
Hans-Carl Oberdalhoff <hans-carl.oberdalhoff@primepulse.de>
2021-11-30 13:33:01 +0100

    list users and guests separately check permissions and show help use mail instead of UPN for guests

--- org/security/list-inactive-users.ps1
+++ org/security/list-inactive-users.ps1
@@ -24,7 +24,24 @@ Connect-RjRbGraph
 # Calculate "last sign in date"
 $lastSignInDate = (get-date) - (New-TimeSpan -Days $days) | Get-Date -Format "yyyy-MM-dd"
 $filter='signInActivity/lastSignInDateTime le ' + $lastSignInDate + 'T00:00:00Z'
+try {
+$userObjects = Invoke-RjRbRestMethodGraph -Resource '/users' -OdFilter $filter -Beta
+} catch {
+  "## Getting list of users and guests failed. Maybe missing permissions?"
+  ""
+    "## Make sure, the following Graph API permissions are present:"
+    "## - Users.Read.All (API)"
+    "## - AuditLog.Read.All (API)"
+    ""
+    $_
+    throw ("Listing users failed.")
+}
 
 "## Inactive Users (No SignIn since at least $Days days.)"
 ""
-Invoke-RjRbRestMethodGraph -Resource '/users' -OdFilter $filter -Beta | Select-Object -Property UserPrincipalName,signInSessionsValidFromDateTime | out-string
+$userObjects | Where-Object { $_.userType -eq "Member" } | Select-Object -Property UserPrincipalName,signInSessionsValidFromDateTime | out-string
+""
+
+"## Inactive Guests (No SignIn since at least $Days days.)"
+""
+$userObjects | Where-Object { $_.userType -eq "Guest" } | Select-Object -Property Mail,signInSessionsValidFromDateTime | out-string
