c0975437a070ce27fb6929b7755d43cfb5fd3299
Christian Wallesch <christian.wallesch@glueckkanja-gab.com>
2022-04-14 13:01:51 +0200

    add list inactive app registrations

---
+++ org/security/list-inactive-app-registrations.ps1
@@ -0,0 +1,43 @@
+<#
+  .SYNOPSIS
+  List App registrations, which had no recent user logons.
+
+  .DESCRIPTION
+  List App registrations, which had no recent user logons.
+
+  .NOTES
+  Permissions
+  MS Graph (API):
+  - Directory.Read.All
+  - Device.Read.All
+
+  .INPUTS
+  RunbookCustomization: {
+        "Parameters": {
+            "CallerName": {
+                "Hide": true
+            }
+        }
+    }
+
+#>
+
+#Requires -Modules @{ModuleName = "RealmJoin.RunbookHelper"; ModuleVersion = "0.6.0" }
+
+param(
+    [ValidateScript( { Use-RJInterface -DisplayName "Days without user logon" } )]
+    [int] $Days = 90,
+    # CallerName is tracked purely for auditing purposes
+    [Parameter(Mandatory = $true)]
+    [string] $CallerName
+)
+
+Connect-RjRbGraph
+
+# Calculate "last sign in date"
+$lastSignInDate = (get-date) - (New-TimeSpan -Days $days) | Get-Date -Format "yyyy-MM-dd"
+$filter='createdDateTime le ' + $lastSignInDate + 'T00:00:00Z'
+
+"## Inactive Applications (No SignIn since at least $Days days):"
+""
+Invoke-RjRbRestMethodGraph -Resource "/auditLogs/SignIns" -OdFilter $filter | Select-Object -Property appDisplayName,createdDateTime | Sort-Object -Property approximateLastSignInDateTime | Group-Object -Property appDisplayName |out-string
