c47e3440d83f4524c5cf3d742cc3bb7b98c25c21
Hans-Carl Oberdalhoff <hans-carl.oberdalhoff@primepulse.de>
2022-05-06 15:18:08 +0200

    merge from CWallesch (new runbook)

---
+++ org/security/list-expiring-app-reg-credentials.ps1
@@ -0,0 +1,73 @@
+<#
+  .SYNOPSIS
+  List expiry date of all AppRegistration  credentials
+
+  .DESCRIPTION
+  List expiry date of all AppRegistration credentials
+
+  .NOTES
+  Permissions: 
+   MS Graph - Application Permission
+    Application.Read.All
+
+  .INPUTS
+  RunbookCustomization: {
+        "Parameters": {
+            "CallerName": {
+                "Hide": true
+            }
+        }
+    }
+
+#>
+
+#Requires -Modules @{ModuleName = "RealmJoin.RunbookHelper"; ModuleVersion = "0.6.0" }
+
+param(
+    [ValidateScript( { Use-RJInterface -DisplayName "Days before credential expiry" } )]
+    [int] $Days = 30,
+    # CallerName is tracked purely for auditing purposes
+    [Parameter(Mandatory = $true)]
+    [string] $CallerName
+)
+
+Connect-RjRbGraph
+[array]$apps = @()
+$apps = Invoke-RjRbRestMethodGraph -Resource "/applications" -FollowPaging
+$apps += Invoke-RjRbRestMethodGraph -Resource "/servicePrincipals" 
+
+$date = Get-Date
+
+foreach ($app in $apps) {
+    if (($app.keyCredentials) -or ($app.passwordCredentials)) {
+    
+        $app.keyCredentials | ForEach-Object {
+            $enddate = [datetime]$_.endDateTime
+            if ((New-TimeSpan -Start $date -End $enddate).days -le $Days) {
+                "## App DisplayName: $($app.displayName)"
+                "## App Id: $($app.appId)"
+                ""
+                "Cert DisplayName: $($_.displayName)"
+                "Cert ID: $($_.keyId)"
+                #"Cert EndDateTime: $($_.endDateTime)"
+
+                "Days left: $((New-TimeSpan -Start $date -End $enddate).days)"
+                ""
+            }
+        }
+        $app.passwordCredentials | ForEach-Object {
+            $enddate = [datetime]$_.endDateTime
+            if ((New-TimeSpan -Start $date -End $enddate).days -le $Days) {
+                "## App DisplayName: $($app.displayName)"
+                "## App Id: $($app.appId)"
+                ""
+                "Client Secret DisplayName: $($_.displayName)"
+                "Client Secret ID: $($_.keyId)"
+                #"Client Secret EndDateTime: $($_.endDateTime)"
+            
+                "Days left: $((New-TimeSpan -Start $date -End $enddate).days)"
+                ""
+            }
+        }
+    }
+}
