b5f8bf383df36d518c98453505af9db779439046
Hans-Carl Oberdalhoff <hans-carl.oberdalhoff@primepulse.de>
2022-06-20 15:55:59 +0200

    new runbook: Assign new AutoPilot GroupTag to a device

---
+++ device/general/change-groupTag.ps1
@@ -0,0 +1,44 @@
+<#
+  .SYNOPSIS
+  Assign a new AutoPilot GroupTag to this device.
+
+  .DESCRIPTION
+  Assign a new AutoPilot GroupTag to this device.
+
+  .INPUTS
+  RunbookCustomization: {
+    "Parameters": {
+      "DeviceId": {
+          "Hide": true
+      }
+    }
+  }
+#>
+
+param (
+    [Parameter(Mandatory = $true)]
+    [string] $DeviceId,
+    [Parameter(Mandatory = $true)]
+    [string] $newGroupTag
+)
+
+Connect-RjRbGraph
+
+$targetDevice = Invoke-RjRbRestMethodGraph -Resource "/devices" -OdFilter "deviceId eq '$DeviceId'"
+
+"## Assigning AutoPilot GroupTag '$newGroupTag' to '$($targetDevice.displayName)'"
+
+$apDevice = Invoke-RjRbRestMethodGraph -Resource "/deviceManagement/windowsAutopilotDeviceIdentities" -OdFilter "azureActiveDirectoryDeviceId eq '$DeviceId'" -ErrorAction SilentlyContinue
+
+if (-not $apDevice) {
+  "## AutoPilot Device for $($targetDevice.displayName) not found. Stopping."
+  throw "not found"
+}
+
+$body = @{
+  groupTag = $newGroupTag
+}
+
+Invoke-RjRbRestMethodGraph -Resource "/deviceManagement/windowsAutopilotDeviceIdentities/$($apDevice.id)/UpdateDeviceProperties" -Method Post -Body $body
+
+"## Successfully updated device '$($targetDevice.displayName)'"
