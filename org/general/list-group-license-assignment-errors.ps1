2744623c33c4ee21f23136237cd9a527feacd9aa
Hans-Carl Oberdalhoff <hans-carl.oberdalhoff@primepulse.de>
2022-07-22 09:55:05 +0200

    new runbook: Report groups that have license assignment errors

---
+++ org/general/list-group-license-assignment-errors.ps1
@@ -0,0 +1,35 @@
+<#
+  .SYNOPSIS
+  Report groups that have license assignment errors
+
+  .DESCRIPTION
+  Report groups that have license assignment errors
+
+  .NOTES
+  Permissions (MS Graph, API)
+  - GroupMember.Read.All
+  - Group.Read.All
+
+#>
+
+#Requires -Modules @{ModuleName = "RealmJoin.RunbookHelper"; ModuleVersion = "0.6.0" }
+
+param(
+    # CallerName is tracked purely for auditing purposes
+    [Parameter(Mandatory = $true)]
+    [string] $CallerName
+)
+
+Connect-RjRbGraph
+
+# Query for license assignment errors
+$result = Invoke-RjRbRestMethodGraph -Resource "/groups" -OdFilter "hasMembersWithLicenseErrors eq true"
+
+if (-not $result) {
+    "## No groups with license assignment errors were found."
+} else {
+    "## The following groups have license assignment errors:"
+    $result | ForEach-Object {
+        "Group: '" + $result.displayName + "', ObjectId: " + $result.id
+    }
+}
