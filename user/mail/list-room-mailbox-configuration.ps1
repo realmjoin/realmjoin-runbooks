09ce57dff3759d8c7969c2f2063773eba8146433
Hans-Carl Oberdalhoff <Hans-Carl.Oberdalhoff@glueckkanja-gab.com>
2022-09-13 15:57:38 +0200

    show "Calendar Processing" config for room mailboxes

--- user/mail/list-room-mailbox-configuration.ps1
+++ user/mail/list-room-mailbox-configuration.ps1
@@ -27,7 +27,7 @@
 
 param (
     [Parameter(Mandatory = $true)] 
-    [ValidateScript( { Use-RJInterface -Type Graph -Entity User -DisplayName "User/Mailbox"} )]
+    [ValidateScript( { Use-RJInterface -Type Graph -Entity User -DisplayName "User/Mailbox" } )]
     [string] $UserName,
     # CallerName is tracked purely for auditing purposes
     [Parameter(Mandatory = $true)]
@@ -38,11 +38,27 @@ Connect-RjRbGraph
 
 $User = Invoke-RjRbRestMethodGraph -Resource "/users/$($UserName)" 
 
+"## Basic Infos:"
 try {
-Invoke-RjRbRestMethodGraph -Resource "/places/$($User.userPrincipalName)/microsoft.graph.room" 
-} catch {
+    Invoke-RjRbRestMethodGraph -Resource "/places/$($User.userPrincipalName)/microsoft.graph.room" 
+}
+catch {
     "## Fetching Room Configuration for '$UserName' failed. Either missing permissions or this is not a Room-Mailbox."
     "## - If this is a Room Mailbox, make sure 'Place.Read.All' permissions are granted."
     ""
     Write-Error $_
 }
+
+""
+"## Calendar Processing:"
+Connect-RjRbExchangeOnline
+try {
+    Get-CalendarProcessing -Identity $UserName | Select-Object -Property AllBookInPolicy, AutomateProcessing, AllowConflicts, BookingWindowsInDays, MaximumDurationInMinutes, ForwardRequetsToDelegates, DeleteSubject, AddOrganizertoSubject, OrganizerInfo
+}
+catch {
+    "## Fetching Room Configuration for '$UserName' failed."
+}
+finally {
+    Disconnect-ExchangeOnline -Confirm:$false -ErrorAction SilentlyContinue | Out-Null
+}
+
