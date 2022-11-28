c525ade479ae0bfcfd2ba1f1644f1ed8736c2ce9
Hans-Carl Oberdalhoff <Hans-Carl.Oberdalhoff@glueckkanja-gab.com>
2022-11-28 15:53:22 +0100

    better output

--- group/security/report-teams-externally-shared-links.ps1
+++ group/security/report-teams-externally-shared-links.ps1
@@ -34,6 +34,9 @@
             },
             "overrideRecipient": {
                 "Hide": true
+            },
+            "groupId": {
+                "Hide": true
             }
         }
     }
