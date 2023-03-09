c5c8f013eb976b12508aa924047d54f21439a785
dayana.hristova <dayana.hristova@glueckkanja-gab.com>
2023-03-09 11:51:50 +0100

    innitial Runbook commit

---
+++ user/general/reprovision-windows365.ps1
@@ -0,0 +1,15 @@
+<#  
+    .SYNOPSIS
+    Reprovision a Windows 365 Cloud PC
+
+    .DESCRIPTION
+    Reprovision an already existing Windows 365 Cloud PC without reassigning a new instance for this user.
+
+    .NOTES
+    Permissions:
+    MS Graph (API):
+    - User.Read.All
+    - GroupMember.ReadWrite.All 
+    - Group.ReadWrite.All
+    - User.SendMail (later iterations)
+#>
