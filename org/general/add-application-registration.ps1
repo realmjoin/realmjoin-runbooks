e1ebe7df4253d6f8846729afc549d9f00e1ecaf1
Hans-Carl Oberdalhoff <Hans-Carl.Oberdalhoff@glueckkanja-gab.com>
2023-05-02 18:21:25 +0200

    Update: Add App. Registration

--- org/general/add-application-registration.ps1
+++ org/general/add-application-registration.ps1
@@ -10,6 +10,7 @@
     Permissions: 
     MS Graph (API):
     - Application.ReadWrite.All
+    - RoleManagement.ReadWrite.Directory
 
 .INPUTS
     RunbookCustomization: {
@@ -30,7 +31,9 @@ param(
     [Parameter(Mandatory = $true)]
     [string] $ApplicationName,
     [string] $signInAudience = "AzureADMyOrg",
-    [int] $ApplicationType = 0,
+    [ValidateScript( { Use-RJInterface -DisplayName "Grant custom role permissions to this user." -Type Graph -Entity User } )]
+    [string] $targetUserId = "",
+    [string] $customPermissionRoleName = "",
     # CallerName is tracked purely for auditing purposes
     [Parameter(Mandatory = $true)]
     [string] $CallerName
@@ -43,7 +46,7 @@ Connect-RjRbGraph
 # Check if an application with the same name already exists
 $existingApp = Invoke-RjRbRestMethodGraph -Method GET -Resource "/applications" -OdFilter "displayName eq '$ApplicationName'"
 
-if ($null -eq $existingApp) {
+if ($existingApp) {
     "## Application '$ApplicationName' already exists, id: $($existingApp.id)"
     "## Skipping application creation"
     Exit
@@ -54,8 +57,44 @@ $body = @{
     "signInAudience" = $signInAudience
 }
 
-$result = Invoke-RjRbRestMethodGraph -Resource "/applications" -Method POST -Body $body
-
+$resultApp = Invoke-RjRbRestMethodGraph -Resource "/applications" -Method POST -Body $body
 ""
-"## Application '$ApplicationName' created, id: $($result.id)"
+"## Application '$ApplicationName' created, Appid: $($resultApp.appId)"
 
+$body = @{
+    "appId" = $resultApp.appId
+}
+$resultSvcPrincipal = Invoke-RjRbRestMethodGraph -Resource "/servicePrincipals" -Method POST -Body $body
+"## Service Principal created."
+
+$roleDefinition = $null
+if ($customPermissionRoleName) {
+    $roleDefinition = Invoke-RjRbRestMethodGraph -Resource "/roleManagement/directory/roleDefinitions/" -OdFilter "displayName eq '$customPermissionRoleName'" -Method GET
+    if ($null -eq $roleDefinition) {
+        ""
+        "## Role definition '$customPermissionRoleName' not found"
+        "## Skipping role assignment"
+        ""
+    }
+}
+
+if ($targetUserId) {
+    $targetUser = Invoke-RjRbRestMethodGraph -Resource "/users/$targetUserId" -ErrorAction SilentlyContinue
+    if ($null -eq $targetUser) {
+        ""
+        "## User '$targetUserPrincipalName' not found"
+        "## Skipping role assignment"
+        ""
+    }
+}
+
+if ($roleDefinition -and $targetUser) {
+    $roleAssignment = @{
+        "principalId" = $targetUser.id
+        "roleDefinitionId" = $roleDefinition.id
+        "directoryScopeId" = "/$($resultApp.id)"
+    }
+
+    $resultApp = Invoke-RjRbRestMethodGraph -Resource "/roleManagement/directory/roleAssignments/" -Method POST -Body $roleAssignment
+    "## Role assignment for '$customPermissionRoleName' to '$($targetUser.displayName)' created."
+}
