bae357652c94e36a93485f52ea0df7f617900778
Ugur Koc | Microsoft MVP <43906965+ugurkocde@users.noreply.github.com>
2024-06-13 16:45:46 +0200

    fix

--- org/general/report-pim-activations.ps1
+++ org/general/report-pim-activations.ps1
@@ -22,7 +22,7 @@ param(
     [Parameter(Mandatory = $true)]
     [string] $CallerName,
     [string] $sendAlertTo = "ugur.koc@glueckkanja.com",
-    [string] $sendAlertFrom = "support@glueckkanja.com"
+    [string] $sendAlertFrom = "support@contoso.com"
 )
 
 Connect-RjRbGraph
