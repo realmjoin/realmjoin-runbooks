5325f204f21f05916bdfebe07b093c9ea6f0c17b
Ugur Koc | Microsoft MVP <43906965+ugurkocde@users.noreply.github.com>
2024-06-18 13:53:25 +0200

    add

--- device/general/outphase-device.ps1
+++ device/general/outphase-device.ps1
@@ -1,14 +1,14 @@
 <#
   .SYNOPSIS
-  Remove/Outphase a windows device
+  Remove/Outphase windows devices in bulk
 
   .DESCRIPTION
-  Remove/Outphase a windows device. You can choose if you want to wipe the device and/or delete it from Intune an AutoPilot.
+  Remove/Outphase windows devices based on a list of serial numbers. You can choose if you want to wipe the devices and/or delete them from Intune and AutoPilot.
 
   .NOTES
   PERMISSIONS
    DeviceManagementManagedDevices.PrivilegedOperations.All (Wipe,Retire / seems to allow us to delete from AzureAD)
-   DeviceManagementManagedDevices.ReadWrite.All (Delete Inunte Device)
+   DeviceManagementManagedDevices.ReadWrite.All (Delete Intune Device)
    DeviceManagementServiceConfig.ReadWrite.All (Delete Autopilot enrollment)
    Device.Read.All
   ROLES
@@ -18,65 +18,69 @@
   RunbookCustomization: {
     
     "Parameters": {
-        "DeviceId": {
-            "Hide": true
+        "SerialNumbers": {
+            "DisplayName": "Serial Numbers",
+            "Type": "String",
+            "IsMultiValue": true,
+            "Required": true
         },
         "intuneAction": {
-            "DisplayName": "Wipe this device?",
+            "DisplayName": "Wipe these devices?",
             "Select": {
                 "Options": [
                     {
-                        "Display": "Completely wipe device (not keeping user or enrollment data)",
+                        "Display": "Completely wipe devices (not keeping user or enrollment data)",
                         "Value": 2
                     },
                     {
-                        "Display": "Delete device from Intune (only if device is already wiped or destroyed)",
+                        "Display": "Delete devices from Intune (only if devices are already wiped or destroyed)",
                         "Value": 1
-                    },                    {
-                        "Display": "Do not wipe or remove device from Intune",
+                    },
+                    {
+                        "Display": "Do not wipe or remove devices from Intune",
                         "Value": 0
                     }
                 ],
                 "ShowValue": false
             }
         },
-        "wipeDevice": {
+        "wipeDevices": {
             "Hide":true
         },
-        "removeIntuneDevice": {
+        "removeIntuneDevices": {
             "Hide":true
         },
-        "removeAutopilotDevice": {
-            "DisplayName": "Delete device from AutoPilot database?",
+        "removeAutopilotDevices": {
+            "DisplayName": "Delete devices from AutoPilot database?",
             "SelectSimple": {
-                "Remove the device from AutoPilot (the device can leave the tenant)": true,
-                "Keep device / do not care": false
+                "Remove the devices from AutoPilot (the devices can leave the tenant)": true,
+                "Keep devices / do not care": false
             }
         },
         "aadAction": {
-            "DisplayName": "Delete device from AzureAD?",
+            "DisplayName": "Delete devices from AzureAD?",
             "Select": {
                 "Options": [
                     {
-                        "Display": "Delete device in AzureAD",
+                        "Display": "Delete devices in AzureAD",
                         "Value": 2
                     },
                     {
-                        "Display": "Disable device in AzureAD",
+                        "Display": "Disable devices in AzureAD",
                         "Value": 1
                     },
                     {
-                        "Display": "Do not delete AzureAD device / do not care",
+                        "Display": "Do not delete AzureAD devices / do not care",
                         "Value": 0
                     }
                 ],
                 "ShowValue": false
             }
         },
-        "removeAADDevice": {
+        "removeAADDevices": {
             "Hide":true
         },
-        "disableAADDevice": {
+        "disableAADDevices": {
             "Hide":true
         },
         "CallerName": {
@@ -90,14 +94,14 @@
 
 param (
     [Parameter(Mandatory = $true)]
-    [string] $DeviceId,
+    [string[]] $SerialNumbers,
     [int] $intuneAction = 2,
     [int] $aadAction = 2,
-    [bool] $wipeDevice = $true,
-    [bool] $removeIntuneDevice = $false,
-    [bool] $removeAutopilotDevice = $true,
-    [bool] $removeAADDevice = $true,
-    [bool] $disableAADDevice = $false,
+    [bool] $wipeDevices = $true,
+    [bool] $removeIntuneDevices = $false,
+    [bool] $removeAutopilotDevices = $true,
+    [bool] $removeAADDevices = $true,
+    [bool] $disableAADDevices = $false,
     # CallerName is tracked purely for auditing purposes
     [Parameter(Mandatory = $true)]
     [string] $CallerName
@@ -108,137 +112,141 @@ Write-RjRbLog -Message "Caller: '$CallerName'" -Verbose
 # only modify parameters, if "actions" are set to non-default values
 switch ($intuneAction) {
     1 {
-        $wipeDevice = $false
-        $removeIntuneDevice = $true
+        $wipeDevices = $false
+        $removeIntuneDevices = $true
     }
     0 {
-        $wipeDevice = $false
-        $removeIntuneDevice = $false
+        $wipeDevices = $false
+        $removeIntuneDevices = $false
     }
 }
 switch ($aadAction) {
     1 {
-        $removeAADDevice = $false
-        $disableAADDevice = $true
+        $removeAADDevices = $false
+        $disableAADDevices = $true
     } 
     0 {
-        $removeAADDevice = $false
-        $disableAADDevice = $false
+        $removeAADDevices = $false
+        $disableAADDevices = $false
     }
 }
 
 Connect-RjRbGraph
 
-# "Searching DeviceId $DeviceID."
-$targetDevice = Invoke-RjRbRestMethodGraph -Resource "/devices" -OdFilter "deviceId eq '$DeviceId'" -ErrorAction SilentlyContinue
-if (-not $targetDevice) {
-    throw ("DeviceId $DeviceId not found in AzureAD.")
-} 
-$owner = Invoke-RjRbRestMethodGraph -Resource "/devices/$($targetDevice.id)/registeredOwners" -ErrorAction SilentlyContinue
+foreach ($serialNumber in $SerialNumbers) {
+    # Searching Device by Serial Number
+    $targetDevice = Invoke-RjRbRestMethodGraph -Resource "/devices" -OdFilter "serialNumber eq '$serialNumber'" -ErrorAction SilentlyContinue
+    if (-not $targetDevice) {
+        Write-RjRbLog -Message "Device with Serial Number $serialNumber not found in AzureAD." -Verbose
+        continue
+    }
+    $owner = Invoke-RjRbRestMethodGraph -Resource "/devices/$($targetDevice.id)/registeredOwners" -ErrorAction SilentlyContinue
 
-"## Outphasing device '$($targetDevice.displayName)' (DeviceId '$DeviceId')"
-if ($owner) {
-    "## Device owner: '$($owner.UserPrincipalName)'"
-}
+    Write-RjRbLog -Message "## Outphasing device '$($targetDevice.displayName)' (Serial Number '$serialNumber')" -Verbose
+    if ($owner) {
+        Write-RjRbLog -Message "## Device owner: '$($owner.UserPrincipalName)'" -Verbose
+    }
 
-if ($disableAADDevice) {
-    # Currentls MS Graph only allows to update windows devices when used "as App" (vs "delegated").
-    if ($targetDevice.operatingSystem -eq "Windows") {
-        "## Disabling $($targetDevice.displayName) (Object ID $($targetDevice.id)) in AzureAD"
-        try {
-            $body = @{
-                "accountEnabled" = $false
+    if ($disableAADDevices) {
+        # Currently MS Graph only allows updating windows devices when used "as App" (vs "delegated").
+        if ($targetDevice.operatingSystem -eq "Windows") {
+            Write-RjRbLog -Message "## Disabling $($targetDevice.displayName) (Object ID $($targetDevice.id)) in AzureAD" -Verbose
+            try {
+                $body = @{
+                    "accountEnabled" = $false
+                }
+                Invoke-RjRbRestMethodGraph -Resource "/devices/$($targetDevice.id)" -Method Patch -Body $body | Out-Null
             }
-            Invoke-RjRbRestMethodGraph -Resource "/devices/$($targetDevice.id)" -Method Patch -Body $body | Out-Null
+            catch {
+                Write-RjRbLog -Message "## Error Message: $($_.Exception.Message)" -Verbose
+                Write-RjRbLog -Message "## Please see 'All logs' for more details." -Verbose
+                Write-RjRbLog -Message "## Execution stopped." -Verbose
+                throw "Disabling Object ID $($targetDevice.id) in AzureAD failed!" 
+            }
+        } else {
+            Write-RjRbLog -Message "## Disabling non-windows devices in AzureAD is currently not supported. Skipping." -Verbose
         }
-        catch {
-            "## Error Message: $($_.Exception.Message)"
-            "## Please see 'All logs' for more details."
-            "## Execution stopped." 
-            throw "Disabling Object ID $($targetDevice.id) in AzureAD failed!" 
-        }
-    } else {
-        "## Disabling non-windows devices in AzureAD is currently not supported. Skipping."
     }
-}
 
-if ($removeAADDevice) {
-    "## Deleting $($targetDevice.displayName) (Object ID $($targetDevice.id)) from AzureAD"
-    try {
-        Invoke-RjRbRestMethodGraph -Resource "/devices/$($targetDevice.id)" -Method Delete | Out-Null
-    }
-    catch {
-        "## Error Message: $($_.Exception.Message)"
-        "## Please see 'All logs' for more details."
-        "## Execution stopped." 
-        throw "Deleting Object ID $($targetDevice.id) from AzureAD failed!"
-        
-    }
-}
-
-if ((-not $disableAADDevice) -and (-not $removeAADDevice)) {
-    "## Skipping AzureAD object operations."
-}
-
-$mgdDevice = Invoke-RjRbRestMethodGraph -Resource "/deviceManagement/managedDevices" -OdFilter "azureADDeviceId eq '$DeviceId'" -ErrorAction SilentlyContinue
-if ($mgdDevice) {
-    if ($wipeDevice) {
-        "## Wiping DeviceId $DeviceID (Intune ID: $($mgdDevice.id))"
-        $body = @{
-            "keepEnrollmentData" = $false
-            "keepUserData"       = $false
-        }
+    if ($removeAADDevices) {
+        Write-RjRbLog -Message "## Deleting $($targetDevice.displayName) (Object ID $($targetDevice.id)) from AzureAD" -Verbose
         try {
-            Invoke-RjRbRestMethodGraph -Resource "/deviceManagement/managedDevices/$($mgdDevice.id)/wipe" -Method Post -Body $body | Out-Null
+            Invoke-RjRbRestMethodGraph -Resource "/devices/$($targetDevice.id)" -Method Delete | Out-Null
         }
         catch {
-            "## Error Message: $($_.Exception.Message)"
-            "## Please see 'All logs' for more details."
-            "## Execution stopped."     
-            throw "Wiping DeviceID $DeviceID (Intune ID: $($mgdDevice.id)) failed!"
+            Write-RjRbLog -Message "## Error Message: $($_.Exception.Message)" -Verbose
+            Write-RjRbLog -Message "## Please see 'All logs' for more details." -Verbose
+            Write-RjRbLog -Message "## Execution stopped." -Verbose
+            throw "Deleting Object ID $($targetDevice.id) from AzureAD failed!"
         }
     }
-    elseif ($removeIntuneDevice) {
-        "## Deleting DeviceId $DeviceID (Intune ID: $($mgdDevice.id)) from Intune"
-        try {
-            Invoke-RjRbRestMethodGraph -Resource "/deviceManagement/managedDevices/$($mgdDevice.id)" -Method Delete | Out-Null
+
+    if ((-not $disableAADDevices) -and (-not $removeAADDevices)) {
+        Write-RjRbLog -Message "## Skipping AzureAD object operations." -Verbose
+    }
+
+    $mgdDevice = Invoke-RjRbRestMethodGraph -Resource "/deviceManagement/managedDevices" -OdFilter "azureADDeviceId eq '$($targetDevice.id)'" -ErrorAction SilentlyContinue
+    if ($mgdDevice) {
+        if ($wipeDevices) {
+            Write-RjRbLog -Message "## Wiping DeviceId $($targetDevice.id) (Intune ID: $($mgdDevice.id))" -Verbose
+            $body = @{
+                "keepEnrollmentData" = $false
+                "keepUserData"       = $false
+            }
+            try {
+                Invoke-RjRbRestMethodGraph -Resource "/deviceManagement/managedDevices/$($mgdDevice.id)/wipe" -Method Post -Body $body | Out-Null
+            }
+            catch {
+                Write-RjRbLog -Message "## Error Message: $($_.Exception.Message)" -Verbose
+                Write-RjRbLog -Message "## Please see 'All logs' for more details." -Verbose
+                Write-RjRbLog -Message "## Execution stopped." -Verbose     
+                throw "Wiping DeviceID $($targetDevice.id) (Intune ID: $($mgdDevice.id)) failed!"
+            }
         }
-        catch {
-            "## Error Message: $($_.Exception.Message)"
-            "## Please see 'All logs' for more details."
-            "## Execution stopped."     
-            throw "Deleting Intune ID: $($mgdDevice.id) from Intune failed!"
+        elseif ($removeIntuneDevices) {
+            Write-RjRbLog -Message "## Deleting DeviceId $($targetDevice.id) (Intune ID: $($mgdDevice.id)) from Intune" -Verbose
+            try {
+                Invoke-RjRbRestMethodGraph -Resource "/deviceManagement/managedDevices/$($mgdDevice.id)" -Method Delete | Out-Null
+            }
+            catch {
+                Write-RjRbLog -Message "## Error Message: $($_.Exception.Message)" -Verbose
+                Write-RjRbLog -Message "## Please see 'All logs' for more details." -Verbose
+                Write-RjRbLog -Message "## Execution stopped." -Verbose     
+                throw "Deleting Intune ID: $($mgdDevice.id) from Intune failed!"
+            }
+        }
+        else {
+            Write-RjRbLog -Message "## Skipping Intune operations." -Verbose
         }
     }
     else {
-        "## Skipping Intune operations."
+        Write-RjRbLog -Message "## Device not found in Intune. Skipping." -Verbose
     }
-}
-else {
-    "## Device not found in Intune. Skipping."
-}
 
-if ($removeAutopilotDevice) {
-    $apDevice = Invoke-RjRbRestMethodGraph -Resource "/deviceManagement/windowsAutopilotDeviceIdentities" -OdFilter "azureActiveDirectoryDeviceId eq '$DeviceId'" -ErrorAction SilentlyContinue
-    if ($apDevice) {
-        "## Deleting DeviceId $DeviceID (Autopilot ID: $($apDevice.id)) from Autopilot"
-        try {
-            Invoke-RjRbRestMethodGraph -Resource "/deviceManagement/windowsAutopilotDeviceIdentities/$($apDevice.id)" -Method Delete | Out-Null
+    if ($removeAutopilotDevices) {
+        $apDevice = Invoke-RjRbRestMethodGraph -Resource "/deviceManagement/windowsAutopilotDeviceIdentities" -OdFilter "azureActiveDirectoryDeviceId eq '$($targetDevice.id)'" -ErrorAction SilentlyContinue
+        if ($apDevice) {
+            Write-RjRbLog -Message "## Deleting DeviceId $($targetDevice.id) (Autopilot ID: $($apDevice.id)) from Autopilot" -Verbose
+            try {
+                Invoke-RjRbRestMethodGraph -Resource "/deviceManagement/windowsAutopilotDeviceIdentities/$($apDevice.id)" -Method Delete | Out-Null
+            }
+            catch {
+                Write-RjRbLog -Message "## Error Message: $($_.Exception.Message)" -Verbose
+                Write-RjRbLog -Message "## Please see 'All logs' for more details." -Verbose
+                Write-RjRbLog -Message "## Execution stopped." -Verbose     
+                throw "Deleting Autopilot ID: $($apDevice.id) from Autopilot failed!"
+            }
         }
-        catch {
-            "## Error Message: $($_.Exception.Message)"
-            "## Please see 'All logs' for more details."
-            "## Execution stopped."     
-            throw "Deleting Autopilot ID: $($apDevice.id) from Autopilot failed!"
+        else {
+            Write-RjRbLog -Message "## Device not found in AutoPilot database. Skipping." -Verbose
         }
     }
     else {
-        "## Device not found in AutoPilot database. Skipping."
+        Write-RjRbLog -Message "## Skipping AutoPilot operations." -Verbose
     }
-}
-else {
-    "## Skipping AutoPilot operations."
+
+    Write-RjRbLog -Message "" -Verbose
+    Write-RjRbLog -Message "## Device $($targetDevice.displayName) with Serial Number $serialNumber successfully removed/outphased." -Verbose
 }
 
-""
-"## Device $($targetDevice.displayName) with DeviceId $DeviceId successfully removed/outphased."
+Write-RjRbLog -Message "## Bulk Outphase process completed." -Verbose
