2e22b6ba79875de0691c3e4bb356d79f4b6e14b7
Tobias Nebel <tobias.nebel@glueckkanja.com>
2024-08-01 14:18:31 +0200

    Add RunbookCustomization comment

--- user/phone/set-teams-phone-user-custom.ps1
+++ user/phone/set-teams-phone-user-custom.ps1
@@ -9,6 +9,9 @@
     Permissions: 
     The connection of the Microsoft Teams PowerShell module is ideally done through the Managed Identity of the Automation account of RealmJoin.
     If this has not yet been set up and the old "Service User" is still stored, the connect is still included for stability reasons. However, it should be switched to Managed Identity as soon as possible.
+    
+    RunbookCustomization:
+    RunbookCustomization is defined separately in the associated RealmJoin portal settings
 
 #>
 
