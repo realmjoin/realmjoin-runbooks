e8d13e7d6de4e0b74edb14645476e39b83644aef
Ugur Koc | Microsoft MVP <43906965+ugurkocde@users.noreply.github.com>
2024-08-07 15:02:24 +0200

    fixes and logging

--- org/general/check-updatable-assets.ps1
+++ org/general/check-updatable-assets.ps1
@@ -44,57 +44,93 @@ Connect-RjRbGraph -Force
 
 if ($DeviceName) {
     Write-RjRbLog -Message "Checking onboarding status for device: $DeviceName" -Verbose
+    "## Checking onboarding status for device: $DeviceName"
 
     # Get Device ID from Microsoft Entra based on Device Name
     Write-RjRbLog -Message "Fetching Device Details for $DeviceName" -Verbose
-    "## Fetching Device Details for $DeviceName"
-    $deviceDetailsUri = "https://graph.microsoft.com/v1.0/devices?`$filter=displayName eq '$DeviceName'"
-    $deviceResponse = Invoke-RjRbRestMethodGraph -Resource "/devices" -OdFilter "displayName eq '$DeviceName'"
-    $deviceId = $deviceResponse.id
+
+    # $deviceDetailsUri = "https://graph.microsoft.com/v1.0/devices?`$filter=displayName eq '$DeviceName'"
+    $deviceResponse = Invoke-RjRbRestMethodGraph -Resource "/devices" -OdFilter "displayName eq '$DeviceName'" -ErrorAction SilentlyContinue
+    $deviceId = $deviceResponse.deviceId
     if ($deviceId) {
         Write-RjRbLog -Message "Device Found! -> Device ID: $deviceId" -Verbose
     } else {
-        Write-RjRbLog -Message "Device Not Found: $DeviceName" -ErrorAction Stop
+        Write-RjRbLog -Message "Device not found: $DeviceName" -ErrorAction Stop
     }
 
-      # Check if device is onboarded
+    # Check if device is onboarded
     Write-RjRbLog -Message "Checking onboarding status for Device ID: $deviceId" -Verbose
-    "## Checking onboarding status for Device ID: $deviceId"
-    $onboardingUri = "https://graph.microsoft.com/beta/admin/windows/updates/updatableAssets"
+    # $onboardingUri = "https://graph.microsoft.com/beta/admin/windows/updates/updatableAssets/$deviceId"
     try {
-        $onboardingResponse = Invoke-RjRbRestMethodGraph -Resource "/admin/windows/updates/updatableAssets" -Method GET -Beta
-        $isOnboarded = $onboardingResponse | Where-Object { $_.id -eq $deviceId }
-        if ($isOnboarded) {
-            Write-Output "Device '$DeviceName' (ID: $deviceId) is onboarded."
+        $onboardingResponse = Invoke-RjRbRestMethodGraph -Resource "/admin/windows/updates/updatableAssets/$deviceId" -Method GET -Beta -ErrorAction SilentlyContinue
+        if ($onboardingResponse) {
+            $status = "Onboarded"
+            $updateCategories = $onboardingResponse.enrollments.updateCategory -join ", "
+            $errors = if ($onboardingResponse.errors) { 
+                ($onboardingResponse.errors | ForEach-Object { $_.reason }) -join ", "
+            } else { 
+                "None"
+            }
+            Write-Output "Device '$DeviceName' (ID: $deviceId) is $status."
+            Write-Output "Update Categories: $updateCategories"
+            Write-Output "Errors: $errors"
         } else {
             Write-Output "Device '$DeviceName' (ID: $deviceId) is not onboarded."
         }
     } catch {
-        Write-Output "Device '$DeviceName' (ID: $deviceId) is not onboarded."
+        $errorResponse = $_.ErrorDetails.Message | ConvertFrom-Json
+        if ($errorResponse.error.code -eq 'NotFound') {
+            Write-Output "Device '$DeviceName' (ID: $deviceId) is not found. (possibly a non-Windows device)."
+        } else {
+            Write-Output "Device '$DeviceName' (ID: $deviceId) is not onboarded."
+            Write-Output "Error: $($errorResponse.error.message)"
+        }
     }
 } elseif ($GroupObjectId) {
     Write-RjRbLog -Message "Checking onboarding status for group members of Group ID: $GroupObjectId" -Verbose
+    "## Checking onboarding status for group members of Group ID: $GroupObjectId"
 
     # Get Group Members
     Write-RjRbLog -Message "Fetching Group Members for Group ID: $GroupObjectId" -Verbose
-    $groupMembersUri = "https://graph.microsoft.com/v1.0/groups/$GroupObjectId/members"
-    $groupMembersResponse = Invoke-RjRbRestMethodGraph -Resource $groupMembersUri
-    $deviceIds = $groupMembersResponse | Where-Object { $_.'@odata.type' -eq '#microsoft.graph.device' } | Select-Object -ExpandProperty id
+    "## Fetching Group Members for Group ID: $GroupObjectId"
 
-    foreach ($deviceId in $deviceIds) {
+    # $groupMembersUri = "https://graph.microsoft.com/v1.0/groups/$GroupObjectId/members"
+    $groupMembersResponse = Invoke-RjRbRestMethodGraph -Resource "/groups/$GroupObjectId/members" -Method GET
+    $deviceObjects = $groupMembersResponse | Where-Object { $_.'@odata.type' -eq '#microsoft.graph.device' } | Select-Object deviceId, displayName
+
+    foreach ($deviceObject in $deviceObjects) {
+        $deviceId = $deviceObject.deviceId
+        $deviceName = $deviceObject.displayName
+
         Write-RjRbLog -Message "Checking onboarding status for Device ID: $deviceId" -Verbose
         "## Checking onboarding status for Device ID: $deviceId"
-        $onboardingUri = "https://graph.microsoft.com/beta/admin/windows/updates/updatableAssets?`$filter=deviceId eq '$deviceId'"
+        # $onboardingUri = "https://graph.microsoft.com/beta/admin/windows/updates/updatableAssets/$deviceId"
         try {
-            $onboardingResponse = Invoke-RjRbRestMethodGraph -Resource "/admin/windows/updates/updatableAssets" -OdFilter "deviceId eq '$deviceId'" -Method GET -Beta
+            $onboardingResponse = Invoke-RjRbRestMethodGraph -Resource "/admin/windows/updates/updatableAssets/$deviceId" -Method GET -Beta -ErrorAction SilentlyContinue
             if ($onboardingResponse) {
-                Write-Output "Device ID: $deviceId is onboarded."
+                $status = "Onboarded"
+                $updateCategories = $onboardingResponse.enrollments.updateCategory -join ", "
+                $errors = if ($onboardingResponse.errors) { 
+                    ($onboardingResponse.errors | ForEach-Object { $_.reason }) -join ", "
+                } else { 
+                    "None"
+                }
+                Write-Output "Device '$deviceName' (ID: $deviceId) is $status."
+                Write-Output "Update Categories: $updateCategories"
+                Write-Output "Errors: $errors"
             } else {
                 Write-Output "Device ID: $deviceId is not onboarded."
             }
         } catch {
-            Write-Output "Device ID: $deviceId is stMethodnot onboarded."
+            $errorResponse = $_.ErrorDetails.Message | ConvertFrom-Json
+            if ($errorResponse.error.code -eq 'NotFound') {
+                Write-Output "Device '$deviceName' (ID: $deviceId) not found (possibly a non-Windows device)."
+            } else {
+                Write-Output "Device '$deviceName' (ID: $deviceId) is not onboarded."
+                Write-Output "Error: $($errorResponse.error.message)"
+            }
         }
+    
     }
 } else {
     Write-Output "Please specify either a DeviceName or a GroupObjectId."
